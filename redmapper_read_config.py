4e75ecb438476331b0909ac12549b1b8b1767def
Robert Harvey <rbharvey@ki-rh53.slac.stanford.edu>
2016-06-23 12:06:06 -0700

    Modified logic.

--- redmapper_read_config.py
+++ redmapper_read_config.py
@@ -1,18 +1,13 @@
 import yaml
 
-def read_simpleconf(filename, defaults=None, required=False):
+def read_simpleconf(filename, defaults=None):
     """
     Docstring goes here
     """
+	outdict = defaults if defaults is not None else {}
+    with open(filename) as f: yaml_data = yaml.load(f)
+    for tag in outdict:
+    	if outdict[tag] is None: assert(tag in yaml_data)
 
-    if required and (defaults is None):
-        print "Error: if required=True defaults must be an input dictionary."
-        return {}
-
-    outdict = defaults if defaults is not None else {}
-
-    with open(filename) as f:
-    	yaml_data = yaml.load(f)
-    	for tag in yaml_data: outdict[tag] = yaml_data[tag]
-
+    for tag in yaml_data: outdict[tag] = yaml_data[tag]
     return outdict
