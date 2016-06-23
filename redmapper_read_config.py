9248881cc182e4db519335433bfdd31982f41279
R. Brett Harvey <rbrettharvey@DN0a22c269.SUNet>
2016-06-23 14:41:18 -0700

    Wrote docstring.

--- redmapper_read_config.py
+++ redmapper_read_config.py
@@ -1,11 +1,24 @@
 import yaml
 
 def read_yaml(filename, defaults=None):
+    
     """
-    Docstring goes here
+    Name:
+        read_yaml
+    Purpose:
+        Read in a YAML file with key/value pairs and put into dict
+    Calling Sequence:
+        outdict = read_simpleconf(filename, defaults=None)
+    Inputs:
+        filename: configuration file
+    Optional Inputs:
+        defaults: Dictionary of required/default fields/values. All fields in 
+                  defaults are required to be specified. If they are set to 
+                  None, they must be specified by filename.
     """
 	
-	outdict = defaults if defaults is not None else {}
+    outdict = defaults if defaults is not None else {}
+    
     with open(filename) as f: yaml_data = yaml.load(f)
     for tag in outdict:
         if outdict[tag] is None:
