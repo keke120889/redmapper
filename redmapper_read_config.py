8df628b98cdf44758bdec116db9be87918c42781
Robert Harvey <rbharvey@ki-rh53.slac.stanford.edu>
2016-06-23 11:34:54 -0700

    First go at configuration file reading.

--- redmapper_read_config.py
+++ redmapper_read_config.py
@@ -1,4 +1,18 @@
 import yaml
 
-def read_simpleconf(filename):
-    pass
+def read_simpleconf(filename, defaults=None, required=False):
+    """
+    Docstring goes here
+    """
+
+    if required and (defaults is None):
+        print "Error: if required=True defaults must be an input dictionary."
+        return {}
+
+    outdict = defaults if defaults is not None else {}
+
+    with open(filename) as f:
+    	yaml_data = yaml.load(f)
+    	for tag in yaml_data: outdict[tag] = yaml_data[tag]
+
+    return outdict
