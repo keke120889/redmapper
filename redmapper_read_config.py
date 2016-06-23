e545668517b1a2fb507a69ee9abf61ba54c6dd5f
Robert Harvey <rbharvey@ki-rh53.slac.stanford.edu>
2016-06-23 12:37:25 -0700

    Added exception handling.

--- redmapper_read_config.py
+++ redmapper_read_config.py
@@ -4,10 +4,14 @@ def read_simpleconf(filename, defaults=None):
     """
     Docstring goes here
     """
+	
 	outdict = defaults if defaults is not None else {}
     with open(filename) as f: yaml_data = yaml.load(f)
     for tag in outdict:
-    	if outdict[tag] is None: assert(tag in yaml_data)
-
+        if outdict[tag] is None:
+            raise ValueError('A value for the required tag \"' 
+                                + tag + '\" must be specified.')
+    
     for tag in yaml_data: outdict[tag] = yaml_data[tag]
+    
     return outdict
