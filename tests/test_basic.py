7e119f17cace1a0547f72925a04becc226e8c900
Brett Harvey <rbharvey@stanford.edu>
2016-06-24 14:00:10 -0700

    saved

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
-
 import unittest
 
+from .context import sample
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
     def test_absolute_truth_and_meaning(self):
         assert True
-
 
 if __name__ == '__main__':
     unittest.main()
