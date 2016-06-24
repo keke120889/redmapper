b99d814c5d50305f7cae3871e6e49f4d667f83b5
Brett Harvey <rbharvey@stanford.edu>
2016-06-24 13:57:57 -0700

    modified style

--- redmapper/runcat.py
+++ redmapper/runcat.py
@@ -4,27 +4,28 @@ import esutil
 import config
 
 
-def run(confdict=None,conffile=None,outbase=None,savemembers=False,mask=False):
+def run(confdict=None, conffile=None, outbase=None, 
+                        savemembers=False, mask=False):
     '''
     docstring
     '''
 
-    if (confdict is None and conffile is None):
+    if (confdict is None) and (conffile is None):
         raise ValueError("Must have one of confdict or conffile")
 
-    if (confdict is not None and conffile is not None):
+    if (confdict is not None) and (conffile is not None):
         raise ValueError("Must have only one of confdict or conffile")
 
     # read in config file
     if conffile is not None:
-        confdict=config.read_config(conffile)
+        confdict = config.read_config(conffile)
 
     # this allows us to override outbase on the call line
-    if (outbase is None):
+    if outbase is None:
         outbase = confdict['outbase']
 
     # read in the input catalog
-    incat = fitsio.read(confdict['catfile'],ext=1)
+    incat = fitsio.read(confdict['catfile'], ext=1)
 
     # etc, etc.
 
