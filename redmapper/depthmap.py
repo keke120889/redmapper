eea612b0cfac028c02ebf1d0cd7ad1eb3ee318c2
Brett Harvey <rbharvey@stanford.edu>
2016-06-28 11:01:51 -0700

    style

--- redmapper/depthmap.py
+++ redmapper/depthmap.py
@@ -3,6 +3,7 @@ import healpy as hp
 import numpy as np
 
 class DepthMap():
+
     def __init__(self,filename):
         # call method to read
         self.filename = filename
@@ -13,17 +14,17 @@ class DepthMap():
         #self.nside = nside
         pass
 
-    def get_depth(self,ra=None,dec=None,theta=None,phi=None,ipring=None):
+    def get_depth(self, ra=None, dec=None, theta=None, phi=None, ip_ring=None):
         # require ra/dec or theta/phi and check
 
         # return depth info
         pass
 
     
-    def calc_maskdepth(self,maskgals,ra,dec,mpcscale):
+    def calc_maskdepth(self, maskgals, ra, dec, mpc_scale):
         # compute ra and dec based on maskgals
-        ras = ra + (maskgals['X']/(mpcscale*3600.))/np.cos(dec*np.pi/180.)
-        decs = dec + maskgals['Y']/(mpcscale*3600.)
+        ras = ra + (maskgals['X']/(mpc_scale*3600.))/np.cos(dec*np.pi/180.)
+        decs = dec + maskgals['Y']/(mpc_scale*3600.)
 
         theta = (90.0 - decs)*np.pi/180.
         phi = ras*np.pi/180.
