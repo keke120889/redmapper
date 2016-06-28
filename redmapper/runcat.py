c42fb347a546fc2c81a6336832206dfa8dc9548f
Brett Harvey <rbharvey@stanford.edu>
2016-06-28 11:02:12 -0700

    more fits reading

--- redmapper/runcat.py
+++ redmapper/runcat.py
@@ -10,22 +10,32 @@ def run(confdict=None, conffile=None, outbase=None,
     docstring
     '''
 
+    # Read configurations from either explicit dictionary or YAML file
     if (confdict is None) and (conffile is None):
         raise ValueError("Must have one of confdict or conffile")
-
     if (confdict is not None) and (conffile is not None):
         raise ValueError("Must have only one of confdict or conffile")
+    if conffile is not None: confdict = config.read_config(conffile)
 
-    # read in config file
-    if conffile is not None:
-        confdict = config.read_config(conffile)
+    # r0, beta = confdict['percolation_r0'], confdict['percolation_beta']
 
-    # this allows us to override outbase on the call line
-    if outbase is None:
-        outbase = confdict['outbase']
+    # This allows us to override outbase on the call line
+    if outbase is None: outbase = confdict['outbase']
 
-    # read in the input catalog
+    # Read in the input catalog
     incat = fitsio.read(confdict['catfile'], ext=1)
+
+    # Read in the background
+    bkg = None # To be implemented
+
+    # Read in red seq parameters
+    zred = None # To be implemented
+
+    # Read in masked galaxies
+    maskgals = fitsio.read(confdict['maskgalfile'], ext=1)
+
+    # 
+
 
     # etc, etc.
 
