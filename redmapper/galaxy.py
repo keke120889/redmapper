53c9c6bafcbf2432f9960dc8028a3b8869a8bfc1
Brett Harvey <rbharvey@stanford.edu>
2016-06-30 15:06:44 -0700

    Implemented basics of Galaxy and Cluster classes.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -1,12 +1,18 @@
 
 
-class Galaxy():
+class Galaxy:
 
-	def __init__(self):
-		pass
+	def __init__(self, ra, dec):
+		self.ra, self.dec = ra, dec
 
 
-class Cluster():
+class Cluster:
 
-	def __init__(self):
-		pass
+	def __init__(self, center):
+		self.center = center
+
+	def ra(self): return self.center.ra
+
+	def dec(self): return self.center.dec
+	
+	
