f2652b16b6421763e3f79dba50fd6b5a399a424d
Brett Harvey <rbharvey@stanford.edu>
2016-07-01 11:17:41 -0700

    Added get_neighbors.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -4,6 +4,7 @@ class Galaxy:
 
 	def __init__(self, ra, dec):
 		self.ra, self.dec = ra, dec
+		self.neighbors = None
 
 
 class Cluster:
@@ -12,7 +13,10 @@ class Cluster:
 		self.center = center
 
 	def ra(self): return self.center.ra
-
 	def dec(self): return self.center.dec
 	
+	def get_neighbors(self, galaxy_list):
+		if self.neighbors: return self.neighbors
+		# else, use htm_find_neighbors
+		
 	
