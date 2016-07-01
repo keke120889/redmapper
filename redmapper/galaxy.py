55f1160580d1d3b980786f1437d66384d10cb1b4
Brett Harvey <rbharvey@stanford.edu>
2016-07-01 11:28:57 -0700

    Moved get_neighbors.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -6,17 +6,22 @@ class Galaxy:
 		self.ra, self.dec = ra, dec
 		self.neighbors = None
 
+	def get_neighbors(self, galaxy_list):
+		if self.neighbors: return self.neighbors
+		# else, use htm_find_neighbors
+
 
 class Cluster:
 
 	def __init__(self, center):
 		self.center = center
+		self.members = None
 
 	def ra(self): return self.center.ra
 	def dec(self): return self.center.dec
-	
-	def get_neighbors(self, galaxy_list):
-		if self.neighbors: return self.neighbors
-		# else, use htm_find_neighbors
 		
+	def get_potential_members(self):
+		self.members = self.center.get_neighbors()
+		return self.members
 	
+	def get_redshift(self): pass
