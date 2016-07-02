d4fb5d636d25374fd4d3718dbad9762986c61050
Brett Harvey <rbharvey@stanford.edu>
2016-07-01 18:23:35 -0700

    Made sure implementations are working, even if just dummy implmentations.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -5,17 +5,19 @@ class Galaxy:
 
     """ docstring """
 
-    def __init__(self, gal_string):
-        self._read_gal(gal_string)
-        self.neighbors = None
+    def __init__(self, gal_tuple):
+        self._read_gal(gal_tuple)
+        self._neighbor_dists = None
 
-    def _read_gal(self, gal_string): pass
+    def _read_gal(self, gal_tuple): pass
 
     # populates dictionary of galaxies to distances from current galaxy
     # but returns just the keys (the galaxies themselves)
     def get_neighbors(self, galaxy_list):
-        if self.neighbors: return self.neighbors.keys()
-        # else, use htm_find_neighbors
+        if self._neighbor_dists: return self._neighbor_dists.keys()
+        # else, use logic of htm_find_neighbors
+        self._neighbor_dists = {} # this is dummy implementation though
+        return self._neighbor_dists.keys()
 
     def distance_from(self, galaxy):
         if self.neighbors and galaxy in self.neighbors: 
@@ -31,19 +33,22 @@ class Cluster:
 
     def __init__(self, clust_tuple):
         self._read_clust(clust_tuple)
-        self.members = None
-        # self.center = None
+        self.member_probs = None 
 
     def _read_clust(self, clust_tuple):
         self.index, self.ra, self.dec, self.z = clust_tuple
+        # following is just dummy implementation to make code work
+        self.center = Galaxy((self.ra, self.dec, self.z))
 
     # def ra(self): return self.center.ra
     # def dec(self): return self.center.dec
 
-    def get_potential_members(self):
-        if self.members: return self.members.keys()
-        self.members = self.center.get_neighbors()
-        return self.members
+    # I envision members to be a dict from member galaxies to 
+    # respective membership probabilities but get_members gets keys
+    def get_members(self):
+        if self.member_probs: return self.member_probs.keys()
+        self.member_probs = {gal:None for gal in self.center.get_neighbors()}
+        return self.member_probs.keys()
 
     def calc_lambda(self): pass
 
