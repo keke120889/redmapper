6c114513d5b23f345a3c1f705acbe3896592621f
Brett Harvey <rbharvey@stanford.edu>
2016-07-06 16:19:39 -0700

    Broken implementation.

--- redmapper/background.py
+++ redmapper/background.py
@@ -1,19 +1,21 @@
 import fitsio
+import numpy as np
 
 
 class Background:
 
-	""" docstring """
+    """ docstring """
 
-	def __init__(self, filename, confdict=None, scale=1.0):
-		self._filename = filename
-		self._read_background(confdict)
+    def __init__(self, filename, confdict=None, scale=1.0):
+        self._filename = filename
+        self._read_background(confdict)
 
-	def _read_background(self, confdict):
-		if confdict is None: pass # populate dictionary
-		# then set attributes
-		self.bkgdict = fitsio.read(self._filename)
+    def _read_background(self, confdict):
+        if confdict is None:
+            pass # populate dictionary
+        # then set attributes
+        bkgdict = fitsio.read(self._filename)
+        self.chisqbins 
 
-
-	def 
+    
 
