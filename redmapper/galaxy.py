920baf446a9413705966f1ed9482ef38d2fa2d67
Brett Harvey <rbharvey@stanford.edu>
2016-07-11 10:31:15 -0700

    Added functionality.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -22,9 +22,10 @@ class GalaxyCatalog(object):
     """ docstring """
 
     def __init__(self, *ndarrays, depth=10):
-        if any([len(ndarr) != len(ndarrays[0]) for ndarr in ndarrays]):
+        if any([len(arr) != len(ndarrays[0]) for arr in ndarrays]):
             raise ValueError("Input arrays must have the same length.")
-        self._gal_ndarrays = list(ndarrays) # do I need to copy?? Dont think so...
+        self._gal_ndarrays = list(ndarrays)
+        self.dtype = itertools.chain([arr.dtype for arr in self._gal_ndarrays])
         self._htm_matcher = None
 
     @classmethod
@@ -37,15 +38,20 @@ class GalaxyCatalog(object):
             return object.__getattribute__(self, attr)
         except AttributeError:  # attr must be a fieldname
             pass
-        for ndarr in self._gal_ndarrays:
-            if attr.upper() in ndarr.dtype.names:
-                return ndarr[attr.upper()]
+        for arr in self._gal_ndarrays:
+            if attr.upper() in arr.dtype.names:
+                return arr[attr.upper()]
         return object.__getattribute__(self, attr)
 
     def __getitem__(self, key):
-        return tuple(itertools.chain([ndarr for ndarr in self._gal_ndarrays]))
+        if isinstance(key, slice):
+            return np.array([self[i] for i in xrange(*key.indices(len(self)))],
+                                dtype=self.dtype)
+        if isinstance(key, list):
+            return np.array([self[i] for i in key, dtype=self.dtype])
+        return tuple(itertools.chain([arr for arr in self._gal_ndarrays]))
 
-    def __len__(self): return len(self._gal_ndarrays)
+    def __len__(self): return len(self._gal_ndarrays[0])
 
     def add_fields(self, ndarray):
         if len(ndarray) != len(self):
@@ -58,56 +64,4 @@ class GalaxyCatalog(object):
         _, indices, dists = self._htm_matcher._match(galaxy.ra, galaxy.dec, 
                                                         radius, maxmatch=-1)
         return indices, dists
-
-
-class Cluster(object):
-
-    """ docstring """
-
-    def __init__(self, clust_tuple):
-        self._read_clust(clust_tuple)
-        self.members = self.centers = None
-
-    def _read_clust(self, clust_tuple):
-        self.index, self.ra, self.dec, self.z = clust_tuple
-
-    def find_members(self, radius=None, galcat=None):
-        if self._member_probs and radius is None:
-            return self.member_probs['GAL']
-        if galcat is None:
-            raise ValueError("A GalaxyCatalog object must be specified.")
-        if radius is None or radius < 0 or radius > 360:
-            raise ValueError("A radius in degrees must be specified.")
-        indices, dists = galaxy_list._match(self.ra, self.dec, radius)
-        ndarray = np.array(np.zeros(len(indices)), 
-                                dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
-
-    def calc_lambda(self): pass
-
-
-class ClusterCatalog(object):
-
-    """ docstring """
-
-    def __init__(self, filename):
-        self._clust_ndarray = fitsio.read(filename)
-        self._clust_list = {}
-
-    def __getattribute__(self, attr):
-        try:
-            return object.__getattribute__(self, attr)
-        except AttributeError:  # attr must be a fieldname
-            pass
-        if attr.upper() in self._clust_ndarray.dtype.names:
-            return self._clust_ndarray[attr.upper()]
-        return object.__getattribute__(self, attr)
-
-    def dtype(self): return self._clust_ndarray.dtype
-
-    def __getitem__(self, key): 
-        if key is not in self._clust_list:
-            self._clust_list[key] = Cluster(self._clust_ndarray[key])
-        return self._clust_list[key]
-
-    def __len__(self): return len(self._clust_ndarray)
 
