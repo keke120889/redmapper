30e1309cefcc91989fac4917ed4bae9507a8d52a
Brett Harvey <rbharvey@stanford.edu>
2016-07-11 10:38:59 -0700

    Fixed ClusterCatalog code. Will ultimately reorganize through inheritance.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -23,9 +23,9 @@ class Cluster(object):
         if radius is None or radius < 0 or radius > 360:
             raise ValueError("A radius in degrees must be specified.")
         indices, dists = galaxy_list._match(self.ra, self.dec, radius)
-        ndarray = np.array(np.zeros(len(indices)), 
+        array = np.array(np.zeros(len(indices)), 
                                 dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
-        self.members = GalaxyCatalog(galcat[indices], ndarray)
+        self.members = GalaxyCatalog(galcat[indices], array)
 
     def calc_lambda(self): pass
 
@@ -34,25 +34,39 @@ class ClusterCatalog(object):
 
     """ docstring """
 
-    def __init__(self, filename):
-        self._clust_ndarray = fitsio.read(filename)
-        self._clust_list = {}
+    def __init__(self, *arrays, depth=10):
+        if any([len(arr) != len(arrays[0]) for arr in arrays]):
+            raise ValueError("Input arrays must have the same length.")
+        self._clust_arrays = list(arrays)
+        self.dtype = itertools.chain([arr.dtype for arr in self._clust_arrays])
+
+    @classmethod
+    def fromfilename(cls, filename):
+        array = fitsio.read(filename)
+        return cls(array)
 
     def __getattribute__(self, attr):
         try:
             return object.__getattribute__(self, attr)
         except AttributeError:  # attr must be a fieldname
             pass
-        if attr.upper() in self._clust_ndarray.dtype.names:
-            return self._clust_ndarray[attr.upper()]
+        for arr in self._clust_arrays:
+            if attr.upper() in arr.dtype.names:
+                return arr[attr.upper()]
         return object.__getattribute__(self, attr)
 
-    def dtype(self): return self._clust_ndarray.dtype
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return np.array([self[i] for i in xrange(*key.indices(len(self)))],
+                                dtype=self.dtype)
+        if isinstance(key, list):
+            return np.array([self[i] for i in key, dtype=self.dtype])
+        return tuple(itertools.chain([arr for arr in self._clust_arrays]))
 
-    def __getitem__(self, key): 
-        if key is not in self._clust_list:
-            self._clust_list[key] = Cluster(self._clust_ndarray[key])
-        return self._clust_list[key]
+    def __len__(self): return len(self._clust_arrays[0])
 
-    def __len__(self): return len(self._clust_ndarray)
+    def add_fields(self, array):
+        if len(array) != len(self):
+            raise ValueError("Input arrays must have the same length.")
+        self._clust_arrays.append(array)
 
