31ce8c7666802ca47199142a1b1d860cfa3ce87e
Brett Harvey <rbharvey@stanford.edu>
2016-07-11 17:04:59 -0700

    Finished reorganization of code, need to test.

--- redmapper/galaxy.py
+++ redmapper/galaxy.py
@@ -2,30 +2,20 @@ import fitsio
 import esutil as eu
 import numpy as np
 import itertools
-from catalog import Catalog
-
-
-class Galaxy(object):
-
-    """ docstring """
-
-    def __init__(self, gal_tuple):
-        self._read_gal(gal_tuple)
-
-    def _read_gal(self, gal_tuple): pass
-
-    def distance_from(self, galaxy):
-        return eu.coords.sphdist(self.ra, self.dec, galaxy.ra, galaxy.dec)
+from catalog import Catalog, DataEntry
 
 
 class GalaxyCatalog(Catalog):
 
-    """ docstring """
+    singleton_class = Galaxy
 
     def __init__(self, *arrays, depth=10):
         super(GalaxyCatalog, self).__init__(arrays)
         self._htm_matcher = None
         self.depth = depth
+
+    @classmethod
+    def from_custom_file(cls, filename): pass
 
     def match(self, galaxy, radius):
         if self._htm_matcher is None:
@@ -33,4 +23,7 @@ class GalaxyCatalog(Catalog):
         _, indices, dists = self._htm_matcher._match(galaxy.ra, galaxy.dec, 
                                                         radius, maxmatch=-1)
         return indices, dists
+
+
+class Galaxy(DataEntry): pass
 
