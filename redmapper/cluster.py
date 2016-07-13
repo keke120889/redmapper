8ce7c70caab99e36484a966db6dfddfbeeca9d3d
Brett Harvey <rbharvey@stanford.edu>
2016-07-13 15:55:14 -0700

    Bug fixes.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -1,6 +1,5 @@
 import fitsio
 import esutil as eu
-from esutil.htm import Matcher
 import numpy as np
 import itertools
 from catalog import Catalog, Entry
@@ -11,12 +10,13 @@ class Cluster(Entry):
     def find_members(self, radius=None, galcat=None):
         if galcat is None:
             raise ValueError("A GalaxyCatalog object must be specified.")
-        if radius is None or radius < 0 or radius > 360:
+        if radius is None or radius < 0 or radius > 180:
             raise ValueError("A radius in degrees must be specified.")
         indices, dists = galcat.match(self, radius)
-        new_fields_array = p.array(np.zeros(len(indices)), 
-                                dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
-        self.members = galcat[indices].add_fields(new_fields_array)
+        self.members = galcat[indices]
+        new_fields_array = np.array(zip(dists, np.zeros(len(indices))),
+                                        dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
+        self.members.add_fields(new_fields_array)
 
     def calc_lambda(self): pass
 
