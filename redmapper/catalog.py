212c6ede3ff4089eb36731a76002e2eee9c40d96
Brett Harvey <rbharvey@stanford.edu>
2016-07-13 15:23:37 -0700

    Bug fixes.

--- redmapper/catalog.py
+++ redmapper/catalog.py
@@ -4,24 +4,18 @@ import numpy as np
 import itertools
 from numpy.lib.recfunctions import merge_arrays
 
-
-class Catalog(object):
+### This should be used as an abstract base class
+class DataObject(object):
 
     """ docstring """
 
-    singleton_class = DataEntry
-
     def __init__(self, *arrays):
-        if any([len(arr) != len(arrays[0]) for arr in arrays]):
-            raise ValueError("Input arrays must have the same length.")
         self._ndarray = merge_arrays(arrays, flatten=True)
-        
+
     @classmethod
     def from_fits_file(cls, filename):
         array = fitsio.read(filename)
         return cls(array)
-
-    def __len__(self): return len(self._ndarray)
 
     def __getattribute__(self, attr):
         try:
@@ -33,45 +27,49 @@ class Catalog(object):
         return object.__getattribute__(self, attr)
 
     def __setattr__(self, attr, val):
-        try:
+        if attr == '_ndarray':
             object.__setattr__(self, attr, val)
-        except AttributeError:
-            pass
-        if attr.upper() in self._ndarray.dtype.names:
-            self._ndarray.__setattr__(self, attr, val)
-        object.__setattr__(self, attr, val)
+        elif attr.upper() in self._ndarray.dtype.names:
+            self._ndarray[attr.upper()] = val
+        else:
+            object.__setattr__(self, attr, val)
 
-    def __getitem__(self, key):
-    	if isinstance(key, int):
-    		return singleton_class(np.array(self._ndarray.__getitem__(key)))
-        return type(self)(self._ndarray.__getitem__(key))
-
-    def __setitem__(self, key, val):
-        self._ndarray.__setitem__(key, val)
+    @property
+    def dtype(self): return self._ndarray.dtype
 
     def add_fields(self, array):
-        if len(array) != len(self):
-            raise ValueError("Input arrays must have the same length.")
-        self._ndarray.append(array)
+        self._ndarray = merge_arrays([self._ndarray, array], flatten=True)
 
 
-class DataEntry(Catalog):
+class Entry(DataObject):
+
+    """ docstring """
 
     def __init__(self, *arrays):
-        if len(arrays[0]) != 1:
+        if any([arr.size != 1 for arr in arrays]):
             raise ValueError("Input arrays must have length one.")
-        super(DataEntry, self).__init__(arrays)
+        super(Entry, self).__init__(*arrays)
 
     @classmethod
     def from_dict(cls, dict): pass
 
     def __getattribute__(self, attr):
-        return super(DataEntry, self).__getattribute__(attr)[0]
+        return super(Entry, self).__getattribute__(attr)
+
+
+class Catalog(DataObject):
+
+    """ docstring """
+
+    entry_class = Entry
+
+    def __len__(self): return len(self._ndarray)
 
     def __getitem__(self, key):
-        raise TypeError("\'" + type(self) + 
-    				            "\' object has no attribute \'__getitem__\'")
+    	if isinstance(key, int):
+    		return self.entry_class(self._ndarray.__getitem__(key))
+        return type(self)(self._ndarray.__getitem__(key))
 
-    def __len__(self):
-        raise TypeError("object of type \'" + type(self) + "\' has no len()")
+    def __setitem__(self, key, val):
+        self._ndarray.__setitem__(key, val)
 
