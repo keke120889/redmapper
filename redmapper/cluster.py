faf02bd1d6545bc0336e445db00b6b23a0d8d1a1
Brett Harvey <rbharvey@stanford.edu>
2016-07-13 23:15:22 -0700

    Updated docstrings.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -7,19 +7,25 @@ from catalog import Catalog, Entry
 
 class Cluster(Entry):
 
+    """ docstring """
+
     def find_members(self, radius=None, galcat=None):
         if galcat is None:
             raise ValueError("A GalaxyCatalog object must be specified.")
         if radius is None or radius < 0 or radius > 180:
             raise ValueError("A radius in degrees must be specified.")
-        indices, dists = galcat.match(self, radius)
+        indices, dists = galcat.match(self, radius) # techincally need to pass in a Galaxy
         self.members = galcat[indices]
-        new_fields_array = np.array(zip(dists, np.zeros(len(indices))),
-                                        dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
-        self.members.add_fields(new_fields_array)
+        new_fields = np.array(zip(dists, np.zeros(len(indices))),
+                                    dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
+        self.members.add_fields(new_fields)
 
     def calc_lambda(self): pass
 
 
-class ClusterCatalog(Catalog): entry_class = Cluster
+class ClusterCatalog(Catalog): 
+
+    """ docstring """
+
+    entry_class = Cluster
 
