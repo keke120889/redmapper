ffb12a46a51d2b1aeb774131ee20f78340b00954
Brett Harvey <rbharvey@stanford.edu>
2016-07-15 16:23:45 -0700

    Minor changes.

--- redmapper/background.py
+++ redmapper/background.py
@@ -7,12 +7,16 @@ class Background(Entry):
     """Docstring."""
 
     def __init__(self, filename, scale=1.0):
-        arr = fitsio.read(filename, ext=1)
-        for fieldname in arr.dtype.names:
-            setattr(self, fieldname.lower(), arr[fieldname])
-        self._init_attrs()
+        imagbinsize, zbinsize, chisqbinsize = 0.01, 0.001, 0.5
+        obkg = Entry.from_fits_file(filename)
+        imagbins = np.arange(obkg.imagrange[0], obkg.imagrange[1], 
+                                                            imagbinsize)
+        sigma_g = np.zeros(shape=(obkg.imagbins.size, obkg.chisqbins.size,
+                                                            obkg.zbins.size))
+        sigma_lng = np.zeros(shape=(obkg.imagbins.size, obkg.lnchisqbins.size,
+                                                            obkg.zbins.size))
+        
 
-    def _init_attrs(self):
-        self.imagbinsize, self.zbinsize, self.chisqbinsize = 0.01, 0.001, 0.5
+
 
         
