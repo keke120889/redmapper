4c6b748060028aac2d25fe3a8c1996211e535978
Brett Harvey <rbharvey@stanford.edu>
2016-07-15 12:58:38 -0700

    _init_attrs method.

--- redmapper/background.py
+++ redmapper/background.py
@@ -10,4 +10,9 @@ class Background(Entry):
         arr = fitsio.read(filename, ext=1)
         for fieldname in arr.dtype.names:
             setattr(self, fieldname.lower(), arr[fieldname])
+        self._init_attrs()
+
+    def _init_attrs(self):
+        self.imagbinsize, self.zbinsize, self.chisqbinsize = 0.01, 0.001, 0.5
+
         
