652fc9190f650c929ea751363a92ca391a2c4a4c
Eli Rykoff <esrykoff@lbl.gov>
2016-07-22 03:23:46 -0700

    first unittest for reading a config file

---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+import unittest
+import numpy.testing as testing
+import numpy as np
+
+import redmapper
+
+class ReadConfigTestCase(unittest.TestCase):
+    def runTest(self):
+        file_name = 'testconfig.yaml'
+        file_path = 'data'
+
+        confdict = redmapper.config.read_config('%s/%s' % (file_path, file_name))
+
+        self.assertEqual(confdict['galfile'],'./data/pixelized/dr8_catalog_test_master_table.fit')
+        self.assertEqual(confdict['specfile'],'./data/dr8_test_spec.fit')
+        # and more...
+
+
+
+if __name__=='__main__':
+    unittest.main()
+#    test_read_config()
