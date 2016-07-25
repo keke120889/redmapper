8b3516ea45881a249ab211f041e4a188c3d671b0
Eli Rykoff <esrykoff@lbl.gov>
2016-07-25 14:36:09 -0700

    test data passes test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -7,15 +7,17 @@ import redmapper
 class SplineTestCase(unittest.TestCase):
     def runTest(self):
         # create test data
+        # these numbers are from redMaPPer 6.3.1, DR8
         xx = np.array([0.05,0.15,0.25,0.35,0.45,0.60],dtype=np.float64)
-        yy = np.array([15.68556786,17.98072052,18.93479919,19.67167091,19.79622269,20.11798096], dtype=np.float64)
+        yy = np.array([15.685568,17.980721,18.934799,19.671671,19.796223,20.117981], dtype=np.float64)
 
         # will want to add error checking and exceptions to CubicSpline
 
         spl=redmapper.utilities.CubicSpline(xx, yy)
         vals=spl(np.array([0.01, 0.44, 0.55, 0.665]))
 
-        testing.assert_almost_equal(vals,np.array([14.6480,19.7928,19.9738,20.3013],dtype=np.float64))
+        # these numbers are also from redMaPPer 6.3.1, DR8
+        testing.assert_almost_equal(vals,np.array([14.648017,19.792828,19.973761,20.301322],dtype=np.float64),decimal=6)
         
 
 
