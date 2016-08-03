fb9a45e0d52d79cf429629777c45dff97ce5a34d
Eli Rykoff <esrykoff@lbl.gov>
2016-08-03 08:47:33 -0700

    reorganization of indices for chisq memory usage; index code

--- redmapper/redsequence.py
+++ redmapper/redsequence.py
@@ -8,19 +8,24 @@ from utilities import CubicSpline
 from utilities import MStar
 
 class RedSequenceColorPar(object):
-    def __init__(self, filename, zbinsize=None, minsig=0.01, fine=False):
+    def __init__(self, filename, zbinsize=None, minsig=0.01, fine=False, zrange=None):
 
         pars,hdr=fitsio.read(filename,ext=1,header=True)
 
         try:
             limmag = hdr['LIMMAG']
-            zrange = np.array([hdr['ZRANGE0'],hdr['ZRANGE1']])
+            if (zrange is None):
+                zrange = np.array([hdr['ZRANGE0'],hdr['ZRANGE1']])
+                    
             alpha = hdr['ALPHA']
             mstar_survey = hdr['MSTARSUR']
             mstar_band = hdr['MSTARBAN']
             ncol = hdr['NCOL']
         except:
             raise ValueError("Missing field from parameter header.")
+
+        if len(zrange) != 2:
+            raise ValueError("zrange must have 2 elements");
 
         if zbinsize is None:
             try:
@@ -60,6 +65,15 @@ class RedSequenceColorPar(object):
         nz = np.round((zrange[1]-zrange[0])/zbinsize).astype('i4')
         self.z = zbinsize*np.arange(nz) + zrange[0]
 
+        # append a high-end overflow bin
+        # for computation this will be the same as the top bin, and at the end
+        # we set it to some absurdly large number.
+        self.z = np.append(self.z,self.z[self.z.size-1])
+        nz=nz+1
+
+        self.zbinsize=zbinsize
+        self.zbinscale=int(1./zbinsize)
+
         ms=MStar(mstar_survey,mstar_band)
         
         refmagbinsize=0.01
@@ -71,6 +85,15 @@ class RedSequenceColorPar(object):
             lumrefmagrange=np.array([12.0,ms(zrange[1])-2.5*np.log10(0.1)])
         self.refmagbins=np.arange(refmagrange[0],refmagrange[1],refmagbinsize,dtype='f8')
         self.lumrefmagbins=np.arange(lumrefmagrange[0],lumrefmagrange[1],refmagbinsize,dtype='f8')
+
+        # and for fast look-ups...
+        self.refmagbins = np.append(self.refmagbins,self.refmagbins[self.refmagbins.size-1])
+        self.lumrefmagbins = np.append(self.lumrefmagbins,self.lumrefmagbins[self.lumrefmagbins.size-1])
+        
+        self.refmagbinsize = refmagbinsize
+        self.refmagbinscale = int(1./refmagbinsize)
+        self.refmaginteger = (self.refmagbins*self.refmagbinscale).astype(np.int64)
+        self.lumrefmaginteger = (self.lumrefmagbins*self.refmagbinscale).astype(np.int64)
 
         # is this an old or new structure?
         if 'PIVOTMAG_Z' in pars.dtype.names:
@@ -92,9 +115,8 @@ class RedSequenceColorPar(object):
         self.pivotmag = spl(self.z)
 
         # c/slope
-        self.c = np.zeros((nz,ncol),dtype=np.float32)
-        self.slope = np.zeros((nz,ncol),dtype=np.float32)
-
+        self.c = np.zeros((nz,ncol),dtype=np.float64)
+        self.slope = np.zeros((nz,ncol),dtype=np.float64)
         for j in xrange(ncol):
             jstring='%02d' % (j)
             spl=CubicSpline(pars[0]['Z'+jstring],pars[0]['C'+jstring])
@@ -103,8 +125,8 @@ class RedSequenceColorPar(object):
             self.slope[:,j] = spl(self.z)
 
         # sigma/covmat
-        self.sigma = np.zeros((ncol,ncol,nz),dtype=np.float32)
-        self.covmat = np.zeros((ncol,ncol,nz),dtype=np.float32)
+        self.sigma = np.zeros((ncol,ncol,nz),dtype=np.float64)
+        self.covmat = np.zeros((ncol,ncol,nz),dtype=np.float64)
             
         # diagonals
         for j in xrange(ncol):
@@ -177,41 +199,52 @@ class RedSequenceColorPar(object):
         self._mstar = ms(self.z)
 
         # luminosity function integrations
-        self.lumnorm = np.zeros((nz,self.lumrefmagbins.size))
+        self.lumnorm = np.zeros((self.lumrefmagbins.size,nz))
         self.alpha = alpha
         for i in xrange(nz):
             f=10.**(0.4*(self.alpha+1.0)*(self._mstar[i]-self.lumrefmagbins))*np.exp(-10.**(0.4*(self._mstar[i]-self.lumrefmagbins)))
-            self.lumnorm[i,:] = refmagbinsize*np.cumsum(f)
+            self.lumnorm[:,i] = refmagbinsize*np.cumsum(f)
             
 
         # lupcorr (annoying!)
-        self.lupcorr = np.zeros((nz,ncol,self.refmagbins.size))
+        #self.lupcorr = np.zeros((ncol,nz,self.refmagbins.size))
+        #self.lupcorr = np.zeros((ncol,self.refmagbins.size,nz))
+        self.lupcorr = np.zeros((self.refmagbins.size,nz,ncol),dtype='f8')
         if (do_lupcorr):
             bnmgy = bvalues*1e9
 
             for i in xrange(nz):
-                mags = np.zeros((nmag,self.refmagbins.size))
-                lups = np.zeros((nmag,self.refmagbins.size))
+                mags = np.zeros((self.refmagbins.size,nmag))
+                lups = np.zeros((self.refmagbins.size,nmag))
 
-                mags[ref_ind,:] = self.refmagbins
+                mags[:,ref_ind] = self.refmagbins
 
                 # go redward
                 for j in xrange(ref_ind+1,nmag):
-                    mags[j,:] = mags[j-1,:] - (self.c[i,j-1]+self.slope[i,j-1]*(mags[ref_ind,:]-self.pivotmag[i]))
+                    mags[:,j] = mags[:,j-1] - (self.c[i,j-1]+self.slope[i,j-1]*(mags[:,ref_ind]-self.pivotmag[i]))
                 # blueward
                 for j in xrange(ref_ind-1,-1,-1):
-                    mags[j,:] = mags[j+1,:] + (self.c[i,j]+self.slope[i,j]*(mags[ref_ind,:]-self.pivotmag[i]))
+                    mags[:,j] = mags[:,j+1] + (self.c[i,j]+self.slope[i,j]*(mags[:,ref_ind]-self.pivotmag[i]))
 
                 # and the luptitude conversion
                 for j in xrange(nmag):
-                    flux = 10.**((mags[j,:]-22.5)/(-2.5))
-                    lups[j,:] = 2.5*np.log10(1.0/bvalues[j]) - np.arcsinh(0.5*flux/bnmgy[j])/(0.4*np.log(10.0))
+                    flux = 10.**((mags[:,j]-22.5)/(-2.5))
+                    lups[:,j] = 2.5*np.log10(1.0/bvalues[j]) - np.arcsinh(0.5*flux/bnmgy[j])/(0.4*np.log(10.0))
 
-                magcol = mags[0:ncol,:] - mags[1:ncol+1,:]
-                lupcol = lups[0:ncol,:] - lups[1:ncol+1,:]
+                magcol = mags[:,0:ncol] - mags[:,1:ncol+1]
+                lupcol = lups[:,0:ncol] - lups[:,1:ncol+1]
 
-                self.lupcorr[i,:,:] = lupcol - magcol
+                #self.lupcorr[i,:,:] = lupcol - magcol
+                #self.lupcorr[:,:,i] = lupcol - magcol
+                self.lupcorr[:,i,:] = lupcol - magcol
 
+        # set top overflow bins to very large number
+        self.z[self.z.size-1] = 1000.0
+        self.zinteger=np.round(self.z*self.zbinscale).astype(np.int64)
+        self.refmagbins[self.refmagbins.size-1] = 1000.0
+        self.refmaginteger = np.round(self.refmagbins*self.refmagbinscale).astype(np.int64)
+        self.lumrefmagbins[self.lumrefmagbins.size-1] = 1000.0
+        self.lumrefmaginteger = np.round(self.lumrefmagbins*self.refmagbinscale).astype(np.int64)
 
         # make this into a catalog
         #super(RedSequenceColorPar, self).__init__(zredstr)
@@ -219,10 +252,43 @@ class RedSequenceColorPar(object):
 
     def mstar(self,z):
         # lookup and return mstar...awesome.
-        ind = np.searchsorted(self.z,z)
-        return self._mstar[ind]
+        #ind = np.searchsorted(self.z,z)
+        zind = self.zindex(z)
+        return self._mstar[zind]
 
+    def zindex(self,z):
+        # return the z index/indices with rounding.
+
+        zind = np.searchsorted(self.zinteger,np.round(np.atleast_1d(z)*self.zbinscale).astype(np.int64))
+        if (zind.size == 1):
+            return np.asscalar(zind)
+        else:
+            return zind
+        
+        # and check for top overflows.  Minimum is always 0
+        #test,=np.where(zind == self.z.size)
+        #if (test.size > 0): zind[test] = self.z.size-1
+
+    def refmagindex(self,refmag):
+        # return the refmag index/indices with rounding
+
+        refmagind = np.searchsorted(self.refmaginteger,np.round(np.atleast_1d(refmag)*self.refmagbinscale).astype(np.int64))
+        if (refmagind.size == 1):
+            return np.asscalar(refmagind)
+        else :
+            return refmagind
+
+    def lumrefmagindex(self,lumrefmag):
+
+        lumrefmagind = np.searchsorted(self.lumrefmaginteger,np.round(np.atleast_1d(lumrefmag)*self.refmagbinscale).astype(np.int64))
+        if (lumrefmagind.size == 1):
+            return np.asscalar(lumrefmagind)
+        else:
+            return lumrefmagind
 
     def calculate_zred(self,blah):
         # I think this can be housed here.  Not urgent.
         pass
+
+    def __repr__(self):
+        return "Representation here."
