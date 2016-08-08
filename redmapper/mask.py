393e9fe619d9c4575e5d4363c716c42524f42f83
Brett Harvey <rbharvey@stanford.edu>
2016-08-08 15:05:05 -0700

    Completed implementations of existing fns.

--- redmapper/mask.py
+++ redmapper/mask.py
@@ -21,53 +21,19 @@ class HPMask(Mask):
         maskinfo = Entry(maskinfo)
         nlim, nside, nest = maskinfo.hpix.size, hdr['NSIDE'], hdr['NEST']
         hpix_ring = maskinfo.hpix if nest != 1 else hp.nest2ring(nside,maskinfo.hpix) # replace none w/ fn call
+        muse = np.arange(nlim)
+        
         if confstr.hpix > 0:
-            hpix_area = TOTAL_SQDEG/(12 * hdr['NSIDE']**2)
-            border = confstr.border + 2*np.sqrt(hpix_area)
-
-            # # which hpix_ring pixels are within confstr.hpix (at confstr.nside)?
-            # # with some border allowance
-            
-            # #theta,phi=hp.pix2ang(nside,hpix_ring)
-            # #vec=hp.ang2vec(theta,phi)
-
-            # # if we ignore the border...
-            # theta,phi=hp.pix2ang(nside,hpix_ring)
-            # hpix_coarse=hp.ang2pix(confstr.nside,theta,phi)
-
-            # muse,=np.where(hpix_coarse == confstr.hpix)
-
-            # ## or...
-
-            # theta,phi=hp.pix2ang(nside,hpix_ring)
-            # ipring_big=hp.ang2pix(confstr.nside,theta,phi)
-            # indices,=np.where(ipring_big == hpix)
-            # if border > 0.0:
-            #   # find the extra boundary around the big pixel,
-            #   # at steps equal to the size of the small (mask) pixels
-            #   boundaries=hp.boundaries(confstr.nside,confstr.hpix,step=nside/confstr.nside)
-            #   # see galaxy.py
-            # to do query_disc
-                # theta,phi is of the center of the big pixel
-
+            border = confstr.border + hp.nside2resol(nside)
             theta, phi = hp.pix2ang(confstr.nside, confstr.hpix)
-            radius = np.nside2resol(confstr.nside)/2. + np.sqrt(2) * border + 
-            # set radius to diagonal radius of pixel + border + cushion
+            radius = np.sqrt(2) * (hp.nside2resol(confstr.nside)/2. + border)
             pixint = hp.query_disc(nside, hp.ang2vec(theta, phi), 
                                         np.radians(radius), inclusive=False)
             muse, = esutil.numpy_util.match(hpix_ring, pixint)
-            
-            
-
-            
-            # more to come
-            #ra, dec = sphere_to_astro(*hp.pix2ang(nside, hpix_ring))
-            # more to come
-            #muse = None
-        else:
-            muse = np.arange(nlim)
+        
         offset, ntot = min(hpix_ring)-1, max(hpix_ring)-min(hpix_ring)+3
-        self.nside, self.offset, self.npix = nside, offset, npix
+        self.nside, self.offset, self.npix = nside, offset, ntot
+        
         try:
             self.fracgood_float = 1
             self.fracgood[hpix_ring-offset] = instr[muse].fracgood
@@ -87,7 +53,7 @@ class HPMask(Mask):
         else:
             gd, = np.where(self.fracgood[ipring_off] > np.random.rand(ras.size))
 
-        radmask=np.zeros(ra.size,dtype=np.bool_)
+        radmask = np.zeros(ra.size,dtype=np.bool_)
         radmask[gd] = True
 
         return radmask
