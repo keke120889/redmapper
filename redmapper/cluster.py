a4be0f03cd70d7ba09ff4d7e372bef7d10a18453
Brett Harvey <rbharvey@stanford.edu>
2016-08-10 15:58:15 -0700

    Added dummy implmentations for calculation of radial and luminosity filters.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -22,18 +22,21 @@ class Cluster(Entry):
         self.members.add_fields(new_fields)
         self.members.dist = dists
 
+    def _calc_radial_profile(self, rscale=0.15): pass
+    def _calc_luminosity(self, zredstr): pass
+
     def _calc_bkg_density(self, bkg, cosmo):
-        mpc_per_deg = np.radians(1.) * cosmo.Dl(0, self.z)
+        mpc_scale = np.radians(1.) * cosmo.Dl(0, self.z)
         sigma_g = bkg.sigma_g_lookup(self.z, self.members.chisq, 
                                                     self.members.refmag)
-        return 2 * np.pi * self.members.r * (sigma_g/mpc_per_deg**2)
+        return 2 * np.pi * self.members.r * (sigma_g/mpc_scale**2)
 
-    def calc_richness(self, zredstr, bkg, r0, beta, cosmo):
+    def calc_richness(self, zredstr, bkg, cosmo, r0=1.0, beta=0.2):
         self.members.r = np.radians(self.members.dist) * cosmo.Dl(0, self.z)
         self.members.chisq = zredstr.calculate_chisq(self.members, self.z)
         rho = chisq_pdf(self.members.chisq, zredstr.ncol)
-        nfw = 0 # two dimensional cluster galaxy density profile (NFW)
-        phi = 0 # cluster luminosity function
+        nfw = self._calc_radial_profile()
+        phi = self._calc_luminosity(zredstr)
         ucounts = (2*np.pi*self.members.r) * nfw * phi * rho
         bcounts = self._calc_bkg_density(bkg, cosmo)
         
