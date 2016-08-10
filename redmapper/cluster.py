41f35a296bd84abee88f0c8a312665bd7c3cbc6c
Brett Harvey <rbharvey@stanford.edu>
2016-08-10 12:22:02 -0700

    Repurposed _calc_bkg_density.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -21,28 +21,17 @@ class Cluster(Entry):
                                     dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
         self.members.add_fields(new_fields)
 
-    def _calc_bkg_density(self, z, r, chisq, refmag, bkg, h0=100.0, allow=False):
-        nchisqbins, chisqbinsize = bkg.chisqbins.size, bkg.chisqbins[1]-bkg.chisqbins[0]
-        nrefmagbins, refmagbinsize = bkg.refmagbins.size, bkg.refmagbins[1]-bkg.refmagbins[0]
-        chisqindex = int((chisq-bkg.chisqbins[0]) * nchisqbins
-                            / (bkg.chisqbins[-1]+chisqbinsize-bkg.chisqbins[0]))
-        refmagindex = int((refmag-bkg.refmagbins[0]) * nrefmagbins
-                            / (bkg.refmagbins[-1]+refmagbinsize-bkg.refmagbins[0]))
-
-        chisqindex[np.where(chisqindex < 0 or chisqindex >= nchisqbins)] = 0
-        imagindex[np.where(imagindex < 0 or imagindex >= nimagbins)] = 0
-
-        
-
+    def _calc_bkg_density(self, bkg):
+        pass
 
     def calc_richness(self, zredstr, bkg, r0, beta, mpc_scale):
         r = self.members.dist * mpc_scale
         chisq = zredstr.calculate_chisq(self.members, self.z)
         rho = chisq_pdf(chisq, zredstr.ncol) # chisq dist with ncol DOF
-        sigma = 0 # two dimensional cluster galaxy density profile (NFW)
+        nfw = 0 # two dimensional cluster galaxy density profile (NFW)
         phi = 0 # cluster luminosity function
-        ucounts = (2*np.pi*r*sigma) * phi * rho
-        bcounts = self._calc_bkg_density(self.z, r, chisq, self.members.refmag, bkg)
+        ucounts = (2*np.pi*r*nfw) * phi * rho
+        bcounts = self._calc_bkg_density(bkg)
         
         w = 0
 
