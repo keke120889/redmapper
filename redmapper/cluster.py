30782952735e90f50c8c5976906d728acd0e5dfc
Brett Harvey <rbharvey@stanford.edu>
2016-08-10 13:55:05 -0700

    Began _calc_bkg_density implementation.

--- redmapper/cluster.py
+++ redmapper/cluster.py
@@ -15,22 +15,24 @@ class Cluster(Entry):
             raise ValueError("A GalaxyCatalog object must be specified.")
         if radius is None or radius < 0 or radius > 180:
             raise ValueError("A radius in degrees must be specified.")
-        indices, dists = galcat.match(self, radius) # techincally need to pass in a Galaxy
+        indices, dists = galcat.match(self, radius) # pass in a Galaxy?
         self.members = galcat[indices]
-        new_fields = np.array(zip(dists, np.zeros(len(indices))),
-                                    dtype=[('DIST', 'f8'), ('PMEM', 'f8')])
+        new_fields = [('DIST', 'f8'), ('R', 'f8'), ('PMEM', 'f8'), 
+                        ('CHISQ', 'f8')]
         self.members.add_fields(new_fields)
+        self.members.dist = dists
 
     def _calc_bkg_density(self, bkg):
-        pass
+        sigma_g = bkg.sigma_g_lookup(self.z, self.members.chisq, 
+                                                    self.members.refmag)
 
     def calc_richness(self, zredstr, bkg, r0, beta, mpc_scale):
-        r = self.members.dist * mpc_scale
-        chisq = zredstr.calculate_chisq(self.members, self.z)
-        rho = chisq_pdf(chisq, zredstr.ncol) # chisq dist with ncol DOF
+        self.members.r = self.members.dist * mpc_scale
+        self.members.chisq = zredstr.calculate_chisq(self.members, self.z)
+        rho = chisq_pdf(self.members.chisq, zredstr.ncol)
         nfw = 0 # two dimensional cluster galaxy density profile (NFW)
         phi = 0 # cluster luminosity function
-        ucounts = (2*np.pi*r*nfw) * phi * rho
+        ucounts = (2*np.pi*self.members.r) * nfw * phi * rho
         bcounts = self._calc_bkg_density(bkg)
         
         w = 0
