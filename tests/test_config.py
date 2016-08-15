9dbae02b1c3f123da6c8a2e438d9439a2b2526cc
Brett Harvey <rbharvey@stanford.edu>
2016-08-15 13:36:16 -0700

    Changed to support new API.

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,16 +5,16 @@ import numpy as np
 import redmapper
 
 class ReadConfigTestCase(unittest.TestCase):
+    
     def runTest(self):
         file_name = 'testconfig.yaml'
         file_path = 'data'
 
-        confdict = redmapper.config.read_config('%s/%s' % (file_path, file_name))
+        confdict = redmapper.config.Configuration('%s/%s' % (file_path, file_name))
 
-        self.assertEqual(confdict['galfile'],'./data/pixelized/dr8_catalog_test_master_table.fit')
-        self.assertEqual(confdict['specfile'],'./data/dr8_test_spec.fit')
+        self.assertEqual(confdict.galfile,'./data/pixelized/dr8_catalog_test_master_table.fit')
+        self.assertEqual(confdict.specfile,'./data/dr8_test_spec.fit')
         # and more...
-
 
 
 if __name__=='__main__':
