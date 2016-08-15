4a8b427dd782cd01ca4c04c73bb41a627f0d688e
Brett Harvey <rbharvey@stanford.edu>
2016-08-15 14:42:01 -0700

    Added lookup testing.

--- tests/test_background.py
+++ tests/test_background.py
@@ -19,7 +19,7 @@ class BackgroundTestCase(unittest.TestCase):
                           '%s/test_dr8_pars.fit' % (file_path))
         bkg = Background('%s/%s' % (file_path, file_name))
 
-
+        # test creation of lookup table
         inputs = [(172,15,64), (323,3,103), (9,19,21), (242,4,87),
                   (70,12,58), (193,6,39), (87,14,88), (337,5,25), (333,8,9)]
         py_outputs = np.array([bkg.sigma_g[idx] for idx in inputs])
@@ -27,6 +27,17 @@ class BackgroundTestCase(unittest.TestCase):
                                 1.4605126, 0.0098356586, 0.79848081, 
                                 0.011284498, 9.3293247, 8.7064905])
         testing.assert_almost_equal(py_outputs, idl_outputs, decimal=1)
+
+        # test functionality of lookup table
+        z = 0.23185321
+        chisq = np.array([0.13315917, 3.57059131, 3.71567741, 2.46307987,
+                          9.16647519, 8.24240144, -1., 1.19503491])
+        refmag = np.array([1000., 15.05129281, 16.81049236, 18.07566359,
+                        19.88279, 15.56617587, 18.55626717, 15.00271158])
+        py_outputs = bkg.sigma_g_lookup(z, chisq, refmag)
+        idl_outputs = np.array([0.0, 0.0012997627, 0.56412143, 6.4126010, 
+                                42.555183, 0.012194233, np.inf, 0.0])
+        testing.assert_almost_equal(py_outputs, idl_outputs, decimal=5)
 
 
 if __name__=='__main__':
