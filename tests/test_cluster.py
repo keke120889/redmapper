322532a03d06e17723b0314d4d4cc3e53df94e20
Brett Harvey <rbharvey@stanford.edu>
2016-08-18 13:15:35 -0700

    Broke into two test cases. Added setUp method for first.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -3,16 +3,28 @@ import numpy.testing as testing
 import numpy as np
 import fitsio
 
-import redmapper
+from redmapper.cluster import Cluster
+from redmapper.galaxy import GalaxyCatalog
 
-class BackgroundTestCase(unittest.TestCase):
+class ClusterFiltersTestCase(unittest.TestCase):
 
-    def test_member_finding(self): pass
     def test_nfw(self): pass
     def test_luminosity(self): pass
     def test_background(self): pass
-    def test_richness(self): pass
+
+    def setUp(self):
+        self.cluster = Cluster(np.empty(0))
+        file_name, file_path = 'test_cluster_members.fit', 'data'
+        self.cluster.members = GalaxyCatalog.from_fits_file(file_path 
+                                                            + '/' + file_name)
+        self.cluster.z = cluster.members.z[0]
         
+
+        
+class ClusterMembersTestCase(unittest.TestCase):
+
+    def test_member_finding(self): pass
+    def test_richness(self): pass
 
 if __name__=='__main__':
         unittest.main()
