0d49a4f1569e4255978d0110b544a0373335b134
Brett Harvey <rbharvey@stanford.edu>
2016-08-18 14:38:51 -0700

    Added Background stub class for ease of testing. Started implementing other test fns.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -3,6 +3,7 @@ import numpy.testing as testing
 import numpy as np
 import fitsio
 
+from esutil.cosmology import Cosmo
 from redmapper.cluster import Cluster
 from redmapper.galaxy import GalaxyCatalog
 from redmapper.background import Background
@@ -21,7 +22,6 @@ class BackgroundStub(Background):
         self.sigma_lng = obkg.sigma_lng
 
 
-
 class ClusterFiltersTestCase(unittest.TestCase):
 
     def test_nfw_filter(self):
@@ -32,16 +32,20 @@ class ClusterFiltersTestCase(unittest.TestCase):
         testing.assert_almost_equal(py_nfw, idl_nfw)
 
     def test_lum_filter(self):
+        test_indices = np.array([47, 19,  0, 30, 22, 48, 34, 19])
         zred_file_name = 'test_dr8_pars.fit'
         zredstr = RedSequenceColorPar(file_path + '/' + zred_file_name)
-        test_indices = 
         py_lum = self._calc_luminosity(zredstr, maxmag)
-        idl_lum = 
-        testing.assert_almost_equal(py_lum, nfw_lum)
+        idl_lum = np.array([])
+        testing.assert_almost_equal(py_lum, idl_lum)
 
     def test_bkg_filter(self):
+        test_indices = np.array([29, 16, 27,  5, 38, 35, 25, 44])
         bkg_file_name = 'test_bkg.fit'
         bkg = BackgroundStub(self.file_path + '/' + bkg_file_name)
+        py_bkg = self._calc_bkg_density(bkg, Cosmo())
+        idl_bkg = np.array([])
+        testing.assert_almost_equal(py_bkg, idl_bkg)
 
     def setUp(self):
         self.cluster = Cluster(np.empty(0))
@@ -49,9 +53,6 @@ class ClusterFiltersTestCase(unittest.TestCase):
         self.cluster.members = GalaxyCatalog.from_fits_file(file_path 
                                                             + '/' + file_name)
         self.cluster.z = cluster.members.z[0]
-        self.bkg = BackgroundStub(file_path + '/' + bkg_file_name)
-        self.zredstr = RedSequenceColorPar(file_path + '/' + zred_file_name)
-
 
         
 class ClusterMembersTestCase(unittest.TestCase):
@@ -59,5 +60,7 @@ class ClusterMembersTestCase(unittest.TestCase):
     def test_member_finding(self): pass
     def test_richness(self): pass
 
+
 if __name__=='__main__':
-        unittest.main()
+    unittest.main()
+
