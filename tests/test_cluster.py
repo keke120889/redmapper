5723760a12064f6a5da5c1a1de47f998598139ee
Brett Harvey <rbharvey@stanford.edu>
2016-08-18 13:33:39 -0700

    Implemented radial filter test. Passes with no float error issues.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -6,9 +6,17 @@ import fitsio
 from redmapper.cluster import Cluster
 from redmapper.galaxy import GalaxyCatalog
 
+NUM_CHECK = 8
+
 class ClusterFiltersTestCase(unittest.TestCase):
 
-    def test_nfw(self): pass
+    def test_nfw(self):
+        test_indices = np.array([46, 38,  1,  1, 11, 24, 25, 16])
+        py_nfw = self._calc_radial_profile()[indices]
+        idl_nfw = np.array([0.29360449, 0.14824243, 0.14721203, 0.14721203, 
+                            0.23459411, 0.31615007, 0.29307860, 0.29737136])
+        testing.assert_almost_equal(py_nfw, idl_nfw)
+
     def test_luminosity(self): pass
     def test_background(self): pass
 
@@ -18,7 +26,7 @@ class ClusterFiltersTestCase(unittest.TestCase):
         self.cluster.members = GalaxyCatalog.from_fits_file(file_path 
                                                             + '/' + file_name)
         self.cluster.z = cluster.members.z[0]
-        
+
 
         
 class ClusterMembersTestCase(unittest.TestCase):
