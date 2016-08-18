2fef227e2d421058c603a416d9226af2fb524e3e
Brett Harvey <rbharvey@stanford.edu>
2016-08-18 13:02:17 -0700

    Added dummy implementations of test functions.

---
+++ tests/test_cluster.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy.testing as testing
+import numpy as np
+import fitsio
+
+import redmapper
+
+class BackgroundTestCase(unittest.TestCase):
+
+    def test_member_finding(self): pass
+    def test_nfw(self): pass
+    def test_luminosity(self): pass
+    def test_background(self): pass
+    def test_richness(self): pass
+        
+
+if __name__=='__main__':
+        unittest.main()
