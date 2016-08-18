16dd2ad95236e1efc5749bb63f11d36a3f39db20
Brett Harvey <rbharvey@stanford.edu>
2016-08-18 14:05:31 -0700

    Added Background stub class for ease of testing. Started implementing other test fns.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -5,27 +5,52 @@ import fitsio
 
 from redmapper.cluster import Cluster
 from redmapper.galaxy import GalaxyCatalog
+from redmapper.background import Background
+from redmapper.redsequence import RedSequenceColorPar
 
-NUM_CHECK = 8
+
+class BackgroundStub(Background):
+
+    def __init__(self, filename):
+        obkg = Entry.from_fits_file(filename)
+        self.refmagbins = obkg.refmagbins
+        self.chisqbins = obkg.chisqbins
+        self.lnchisqbins = obkg.lnchisqbins
+        self.zbins = obkg.zbins
+        self.sigma_g = obkg.sigma_g
+        self.sigma_lng = obkg.sigma_lng
+
+
 
 class ClusterFiltersTestCase(unittest.TestCase):
 
-    def test_nfw(self):
+    def test_nfw_filter(self):
         test_indices = np.array([46, 38,  1,  1, 11, 24, 25, 16])
-        py_nfw = self._calc_radial_profile()[indices]
+        py_nfw = self._calc_radial_profile()[test_indices]
         idl_nfw = np.array([0.29360449, 0.14824243, 0.14721203, 0.14721203, 
                             0.23459411, 0.31615007, 0.29307860, 0.29737136])
         testing.assert_almost_equal(py_nfw, idl_nfw)
 
-    def test_luminosity(self): pass
-    def test_background(self): pass
+    def test_lum_filter(self):
+        zred_file_name = 'test_dr8_pars.fit'
+        zredstr = RedSequenceColorPar(file_path + '/' + zred_file_name)
+        test_indices = 
+        py_lum = self._calc_luminosity(zredstr, maxmag)
+        idl_lum = 
+        testing.assert_almost_equal(py_lum, nfw_lum)
+
+    def test_bkg_filter(self):
+        bkg_file_name = 'test_bkg.fit'
+        bkg = BackgroundStub(self.file_path + '/' + bkg_file_name)
 
     def setUp(self):
         self.cluster = Cluster(np.empty(0))
-        file_name, file_path = 'test_cluster_members.fit', 'data'
+        self.file_path, file_name = 'data', 'test_cluster_members.fit'
         self.cluster.members = GalaxyCatalog.from_fits_file(file_path 
                                                             + '/' + file_name)
         self.cluster.z = cluster.members.z[0]
+        self.bkg = BackgroundStub(file_path + '/' + bkg_file_name)
+        self.zredstr = RedSequenceColorPar(file_path + '/' + zred_file_name)
 
 
         
