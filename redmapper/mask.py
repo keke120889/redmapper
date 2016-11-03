91f9215d685bb74e639b4b1fdd6612dd32b80c59
Eli Rykoff <erykoff@slac.stanford.edu>
2016-11-02 22:49:10 -0700

    add documentation

--- redmapper/mask.py
+++ redmapper/mask.py
@@ -6,8 +6,21 @@ from utilities import TOTAL_SQDEG, SEC_PER_DEG, astro_to_sphere
 
 
 class Mask(object):
-    """Docstring."""
-    
+    """
+    A super-class for (pixelized) footprint masks
+
+    This should not be instantiated directly (yet).
+
+    parameters
+    ----------
+    confstr: Config object
+       configuration
+    """
+
+    # note: not sure how to organize this.
+    #   We need a routine that looks at the mask_mode and instantiates
+    #   the correct type.  How is this typically done?
+
     def __init__(self, confstr):
         try:
             self.read_maskgals(confstr.maskgalfile)
@@ -18,12 +31,19 @@ class Mask(object):
     def calc_radmask(self, *args, **kwargs): pass
     def read_maskgals(self, maskgalfile):
         self.maskgals = Catalog.from_fits_file(maskgalfile)
-    def gen_maskgals(self): pass
+    def gen_maskgals(self):
+        # this needs to be written to generate maskgals if not from file
+        pass
 
 
 class HPMask(Mask):
-    """HPMask is a HealPix mask and extends the Mask class.
-    
+    """
+    A class to use a healpix mask (mask_mode == 3)
+
+    parameters
+    ----------
+    confstr: Config object
+        Configuration object with maskfile
 
     """
 
@@ -34,7 +54,8 @@ class HPMask(Mask):
         nlim, nside, nest = maskinfo.hpix.size, hdr['NSIDE'], hdr['NEST']
         hpix_ring = maskinfo.hpix if nest != 1 else hp.nest2ring(nside, maskinfo.hpix)
         muse = np.arange(nlim)
-        
+
+        # if we have a sub-region of the sky, cut down the mask to save memory
         if confstr.hpix > 0:
             border = confstr.border + hp.nside2resol(nside)
             theta, phi = hp.pix2ang(confstr.nside, confstr.hpix)
@@ -42,13 +63,14 @@ class HPMask(Mask):
             pixint = hp.query_disc(nside, hp.ang2vec(theta, phi), 
                                         np.radians(radius), inclusive=False)
             muse, = esutil.numpy_util.match(hpix_ring, pixint)
-        
+
         offset, ntot = min(hpix_ring)-1, max(hpix_ring)-min(hpix_ring)+3
         self.nside, self.offset, self.npix = nside, offset, ntot
 
         ntot = np.max(hpix_ring) - np.min(hpix_ring) + 3
         self.fracgood = np.zeros(ntot,dtype='f4')
 
+        # check if we have a fracgood in the input maskinfo
         try:
             self.fracgood_float = 1
             self.fracgood[hpix_ring-offset] = maskinfo[muse].fracgood
@@ -58,16 +80,52 @@ class HPMask(Mask):
         super(HPMask, self).__init__(confstr)
 
     def compute_radmask(self, ra, dec):
+        """
+        Determine if a given set of ra/dec points are in or out of mask
+
+        parameters
+        ----------
+        ra: array of doubles
+        dec: array of doubles
+
+        returns
+        -------
+        radmask: array of booleans
+
+        """
+        _ra  = np.atleast_1d(ra)
+        _dec = np.atleast_1d(dec)
+
+        if (_ra.size != _dec.size):
+            raise ValueError("ra, dec must be same length")
+
         theta, phi = astro_to_sphere(ra, dec)
         ipring = hp.ang2pix(self.nside, theta, phi)
         ipring_offset = np.clip(ipring - maskstr.offset, 0, maskstr.npix-1)
-        ref = 0 if self.fracgood_float == 0 else np.random.rand(ras.size)     
+        ref = 0 if self.fracgood_float == 0 else np.random.rand(ras.size)
         radmask = np.zeros(ra.size, dtype=np.bool_)
         radmask[np.where(self.fracgood[ipring_offset] > ref)] = True
         return radmask
-            
-    def set_radmask(self, clusters, mpcscale):
-        ras = clusters.ra + self.maskgals.x/(mpcscale*SEC_PER_DEG)/np.cos(np.radians(clusters.dec))
-        decs = clusters.dec + self.maskgals.y/(mpcscale*SEC_PER_DEG)
+
+    def set_radmask(self, cluster, mpcscale):
+        """
+        Assign mask (0/1) values to maskgals for a given cluster
+
+        parameters
+        ----------
+        cluster: Cluster object
+        mpcscale: float
+            scaling to go from mpc to degrees (check units) at cluster redshift
+
+        results
+        -------
+        sets maskgals['MASKED']
+
+        """
+
+        # note this probably can be in the superclass, no?
+
+        ras = cluster.ra + self.maskgals.x/(mpcscale*SEC_PER_DEG)/np.cos(np.radians(clusters.dec))
+        decs = cluster.dec + self.maskgals.y/(mpcscale*SEC_PER_DEG)
         self.maskgals['MASKED'] = self.compute_radmask(ras,decs)
 
