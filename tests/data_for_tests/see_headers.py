bcfc3662e61c117569e276dbdae1a5145fb09aea
Tom McClintock <tmcclintock@email.arizona.edu>
2017-01-06 11:55:00 -0700

    Trying to get test_cluster.py to set up correctly. See the setUp() function for the current issue with the galcat.

--- tests/data_for_tests/see_headers.py
+++ tests/data_for_tests/see_headers.py
@@ -7,11 +7,12 @@ tests themselves.
 
 import fitsio
 
-filename = "test_cluster_members.fit"
+#filename = "test_cluster_members.fit"
 #filename = "testgals_chisq_mode0.fit"
-#filename = "pixelized_dr8_test/dr8_test_galaxies_master_table.fit"
+filename = "pixelized_dr8_test/dr8_test_galaxies_master_table.fit"
 #filename = "pixelized_dr8_test/dr8_test_galaxies_0008421.fit"
 data,h = fitsio.read(filename,header=True)
 out = data.dtype.names
+print filename
 for i in range(len(out)):
     print out[i]
