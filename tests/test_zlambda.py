5f6f20862f0d29c9be376ee425b4dffa40f549a1
Eli Rykoff <erykoff@slac.stanford.edu>
2018-01-17 16:39:00 -0800

    Cleanup

--- tests/test_zlambda.py
+++ tests/test_zlambda.py
@@ -15,33 +15,33 @@ from redmapper.depthmap import DepthMap
 class ClusterFiltersTestCase(unittest.TestCase):
     """
     Unittest for testing the z_lambda functions
-    
+
     INCOMPLETE
-    
+
     """
     def runTest(self):
-        
+
         self.file_path = 'data_for_tests'
-        
+
         #The configuration
         #Used by the mask, background and richness calculation
-        
+
         self.cluster = Cluster()
-        
+
         conf_filename = 'testconfig.yaml'
         confstr = Configuration(self.file_path + '/' + conf_filename)
-        
+
         filename = 'test_cluster_members.fit'
         neighbors = GalaxyCatalog.from_fits_file(self.file_path + '/' + filename)
-        
+
         zred_filename = 'test_dr8_pars.fit'
         zredstr = RedSequenceColorPar(self.file_path + '/' + zred_filename,fine = True)
-        
+
         bkg_filename = 'test_bkg.fit'
         bkg = Background('%s/%s' % (self.file_path, bkg_filename))
-        
+
         self.cluster = Cluster(confstr = confstr, zredstr = zredstr, bkg = bkg, neighbors = neighbors)
-        
+
         hdr=fitsio.read_header(self.file_path+'/'+filename,ext=1)
         self.cluster.z = hdr['Z_LAMBDA']
         self.richness_compare = hdr['LAMBDA']
@@ -49,35 +49,34 @@ class ClusterFiltersTestCase(unittest.TestCase):
         self.cluster.z = self.cluster.neighbors.z[0]
         self.cluster.ra = hdr['RA']
         self.cluster.dec = hdr['DEC']
-        
+
         #Set up the mask
         mask = HPMask(self.cluster.confstr) #Create the mask
-        
+
         mpc_scale = np.radians(1.) * self.cluster.cosmo.Dl(0, self.cluster.z) / (1 + self.cluster.z)**2
         mask.set_radmask(self.cluster, mpc_scale)
-        
+
         #depthstr
         depthstr = DepthMap(self.cluster.confstr)
         depthstr.calc_maskdepth(mask.maskgals, self.cluster.ra, self.cluster.dec, mpc_scale)
-        
+
         self.cluster.neighbors.dist = np.degrees(self.cluster.neighbors.r/self.cluster.cosmo.Dl(0,self.cluster.z))
-        
+
         #set seed
         seed = 0
         random.seed(seed = seed)
-        
+
         #TEST AGAINST CHANGES
         #with calcpz = True
-        z_lambda = self.cluster.redmapper_zlambda(self.cluster.z, mask, calcpz = True)
-        
+        z_lambda = self.cluster.calc_zlambda(self.cluster.z, mask, calcpz = True)
+
         testing.assert_almost_equal(self.cluster.z_lambda, 0.22816455)
         testing.assert_almost_equal(self.cluster.z_lambda_err, 0.00632459)
-        
+
         #test zlambda_err extra
-        z_lambda_err = self.cluster.zlambda_err(self.cluster.z_lambda)
-        
+        z_lambda_err = self.cluster._zlambda_calc_err(self.cluster.z_lambda)
+
         testing.assert_almost_equal(z_lambda_err, 0.00897011)
-        
-        
+
 if __name__=='__main__':
-    unittest.main() 
+    unittest.main()
