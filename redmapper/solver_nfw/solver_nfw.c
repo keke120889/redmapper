b1052dabaa19198aba717139f5fb1226c36a6f86
Eli Rykoff <erykoff@slac.stanford.edu>
2018-01-23 13:18:26 -0800

    Only update numbers if needed

--- redmapper/solver_nfw/solver_nfw.c
+++ redmapper/solver_nfw/solver_nfw.c
@@ -17,30 +17,30 @@ int solver_nfw(double r0, double beta, long ngal,
   lamlo=0.5;
   lamhi=2000.0;
   outlo=-1.0;
-  
+
   while (fabs(lamhi-lamlo) > 2*tol) {
     mid=(lamhi+lamlo)/2.0;
     if (outlo < 0.0) {
-      nfw_weights(lamlo,r0,beta,ngal,ucounts,bcounts,r,w,p,wt,rlambda,theta_r,rsig);
+        nfw_weights(lamlo,r0,beta,ngal,ucounts,bcounts,r,w,p,wt,rlambda,theta_r,rsig,1);
       outlo=0.0;
       for (i=0;i<ngal;i++) {
 	outlo+=wt[i];
       }
       cval = cpars[3] + cpars[2]*(*rlambda) + cpars[1]*(*rlambda)*(*rlambda) + cpars[0]*(*rlambda)*(*rlambda)*(*rlambda);
-      //if (cval < 0.0) { cval = 0.0; }
-      
+      if (cval < 0.0) { cval = 0.0; }
+
       outlo += lamlo*cval;
     }
-    nfw_weights(mid,r0,beta,ngal,ucounts,bcounts,r,w,p,wt,rlambda,theta_r,rsig);
+    nfw_weights(mid,r0,beta,ngal,ucounts,bcounts,r,w,p,wt,rlambda,theta_r,rsig,1);
     outmid=0.0;
     for (i=0;i<ngal;i++) {
       outmid+=wt[i];
     }
     cval = cpars[3] + cpars[2]*(*rlambda) + cpars[1]*(*rlambda)*(*rlambda) + cpars[0]*(*rlambda)*(*rlambda)*(*rlambda);
-    //if (cval < 0.0) { cval = 0.0; }
-    
+    if (cval < 0.0) { cval = 0.0; }
+
     outmid += mid*cval;
-    
+
     if (outlo < 1.0) { outlo = 0.9;} // stability at low end
     if ((outlo-lamlo)*(outmid-mid) > 0.0) {
       lamlo=mid;
@@ -50,11 +50,20 @@ int solver_nfw(double r0, double beta, long ngal,
     }
   }
 
+  // lambda is the midpoint of the two
   *lambda = (lamlo+lamhi)/2.0;
+
+  // and final computation of nfw_weights to update values with final lambda
+  // at the moment, this will not update p, wt because that's not what happens
+  // in the IDL code.  But the IDL code could/should be updated because there
+  // is a discrepancy at the nth decimal place between the probabilities computed
+  // at "mid" and the final lambda.
+  nfw_weights(*lambda,r0,beta,ngal,ucounts,bcounts,r,w,p,wt,rlambda,theta_r,rsig,0);
 
   if (*lambda < 1.0) {
     *lambda = -1.0;
+    *rlambda = -1.0;
   }
-  
+
   return 0;
 }
