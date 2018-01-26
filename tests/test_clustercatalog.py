f8ab1f2b8cac846c2732e488941f7ad665b544ad
Eli Rykoff <erykoff@slac.stanford.edu>
2018-01-26 11:29:46 -0800

    New tests for galaxy catalogs and cluster catalogs

---
+++ tests/test_clustercatalog.py
@@ -0,0 +1,90 @@
+import unittest
+import numpy.testing as testing
+import numpy as np
+import fitsio
+from numpy import random
+import healpy as hp
+from esutil.cosmology import Cosmo
+
+from redmapper.cluster import Cluster
+from redmapper.cluster import ClusterCatalog
+from redmapper.config import Configuration
+from redmapper.galaxy import GalaxyCatalog
+from redmapper.catalog import DataObject
+from redmapper.redsequence import RedSequenceColorPar
+from redmapper.background import Background
+from redmapper.mask import HPMask
+from redmapper.depthmap import DepthMap
+
+
+
+class ClusterCatalogTestCase(unittest.TestCase):
+    """
+    """
+    def runTest(self):
+
+        file_path = 'data_for_tests'
+        conffile = 'testconfig.yaml'
+
+        confstr = Configuration(file_path + '/' + conffile)
+
+        gals_all = GalaxyCatalog.from_galfile(confstr.galfile)
+
+        zred_filename = 'test_dr8_pars.fit'
+        zredstr = RedSequenceColorPar(file_path + '/' + zred_filename, fine=True)
+
+        bkg_filename = 'test_bkg.fit'
+        bkg = Background('%s/%s' % (file_path, bkg_filename))
+
+        mask = HPMask(confstr)
+        depthstr = DepthMap(confstr)
+
+        cosmo = Cosmo()
+
+
+        testcatfile = 'test_cluster_pos.fit'
+        cat = ClusterCatalog.from_catfile(file_path + '/' + testcatfile,
+                                          zredstr=zredstr,
+                                          confstr=confstr,
+                                          bkg=bkg,
+                                          cosmo=cosmo)
+
+        # test single neighbors...
+        c0 = cat[0]
+        c0.find_neighbors(0.2, gals_all)
+        c1 = cat[1]
+        c1.find_neighbors(0.2, gals_all)
+
+        testing.assert_equal(c0.neighbors.size, 580)
+        testing.assert_equal(c1.neighbors.size, 298)
+        testing.assert_array_less(c0.neighbors.dist, 0.2)
+        testing.assert_array_less(c1.neighbors.dist, 0.2)
+
+        # and multi-match...
+        i0, i1, dist = gals_all.match_many(cat.ra, cat.dec, 0.2)
+
+        u0, = np.where(i0 == 0)
+        testing.assert_equal(c0.neighbors.size, u0.size)
+
+        u1, = np.where(i0 == 1)
+        testing.assert_equal(c1.neighbors.size, u1.size)
+
+        # and compute the richness on the first one...
+        mpc_scale = np.radians(1.) * c0.cosmo.Dl(0, c0.z) / (1 + c0.z)**2
+        mask.set_radmask(c0, mpc_scale)
+
+        depthstr.calc_maskdepth(mask.maskgals, c0.ra, c0.dec, mpc_scale)
+
+        richness = c0.calc_richness(mask)
+
+        # Make sure the numbers were propagated to the parent catalog
+        testing.assert_equal(richness, cat.Lambda[0])
+        testing.assert_equal(c0.Lambda_e, cat.Lambda_e[0])
+        testing.assert_equal(c0.scaleval, cat.scaleval[0])
+
+        # And make sure the numbers are correct
+        testing.assert_almost_equal(richness, 23.86299324)
+
+        # and we're done
+        
+
---
+++ tests/test_galaxycatalog.py
@@ -0,0 +1,58 @@
+import unittest
+import numpy.testing as testing
+import numpy as np
+import fitsio
+from numpy import random
+import healpy as hp
+
+from redmapper.config import Configuration
+from redmapper.galaxy import GalaxyCatalog
+
+
+class GalaxyCatalogTestCase(unittest.TestCase):
+    """
+    """
+    def runTest(self):
+
+        file_path = 'data_for_tests'
+
+        galfile = 'pixelized_dr8_test/dr8_test_galaxies_master_table.fit'
+
+        gals_all = GalaxyCatalog.from_galfile(file_path + '/' + galfile)
+
+        # check that we got the expected number...
+        testing.assert_equal(gals_all.size, 14449)
+
+        # read in a subregion, no border
+        gals_sub = GalaxyCatalog.from_galfile(file_path + '/' + galfile,
+                                              hpix=2163, nside=64)
+
+        theta = (90.0 - gals_all.dec) * np.pi/180.
+        phi = gals_all.ra * np.pi/180.
+        ipring_all = hp.ang2pix(64, theta, phi)
+        use, = np.where(ipring_all == 2163)
+
+        testing.assert_equal(gals_sub.size, use.size)
+
+        # read in a subregion, with border
+        gals_sub = GalaxyCatalog.from_galfile(file_path + '/' + galfile,
+                                              hpix=9218, nside=128, border=0.1)
+
+        # this isn't really a big enough sample catalog to fully test...
+        testing.assert_equal(gals_sub.size, 7950)
+
+        # and test the matching...
+
+        indices, dists = gals_all.match_one(140.5, 65.0, 0.2)
+        testing.assert_equal(indices.size, 521)
+        testing.assert_array_less(dists, 0.2)
+
+        i0, i1, dists = gals_all.match_many([140.5,141.2],
+                                            [65.0, 65.2], [0.2,0.1])
+        testing.assert_equal(i0.size, 666)
+        test, = np.where(i0 == 0)
+        testing.assert_equal(test.size, 521)
+        testing.assert_array_less(dists[test], 0.2)
+        test, = np.where(i0 == 1)
+        testing.assert_equal(test.size, 666 - 521)
+        testing.assert_array_less(dists[test], 0.1)
