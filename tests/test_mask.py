51e833edd8cf8683b259bb752421dd6e949c2066
Eli Rykoff <erykoff@slac.stanford.edu>
2018-01-30 07:28:39 -0800

    New test values

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -3,8 +3,9 @@ import numpy.testing as testing
 import numpy as np
 import fitsio
 
-from redmapper.mask import Mask,HPMask
-from redmapper.config import Configuration
+#from redmapper.mask import Mask,HPMask
+from redmapper.mask import get_mask, Mask, HPMask
+from redmapper.configuration import Configuration
 
 class MaskTestCase(unittest.TestCase):
     def runTest(self):
@@ -34,54 +35,47 @@ class MaskTestCase(unittest.TestCase):
         """
         file_path = "data_for_tests"
         conf_filename = "testconfig.yaml"
-        confstr = Configuration(file_path + "/" + conf_filename)
+        config = Configuration(file_path + "/" + conf_filename)
 
-        # Test the read_maskgals() and the gen_maskgals() functions
-        # of the Mask superclass.
-        mask = Mask(confstr)
-        mask.read_maskgals(confstr.maskgalfile)
-        testing.assert_equal(hasattr(mask,'maskgals'),True)
-        # Clear the maskgals attribute and then run gen_maskgals()
-        #delattr(mask,'maskgals')
-        #mask.gen_maskgals()
-        testing.assert_equal(hasattr(mask,'maskgals'),True)
-        
-        # Healpix mask and see if it has maskgals
-        mask = HPMask(confstr)
-        testing.assert_equal(hasattr(mask,'maskgals'),True)
+        # First, test a bare mask
 
-        # First test the healpix configuration
+        config.mask_mode = 0
+        mask = get_mask(config)
+        testing.assert_equal(hasattr(mask, 'maskgals'), True)
+        testing.assert_equal(isinstance(mask, Mask), True)
+        testing.assert_equal(isinstance(mask, HPMask), False)
+
+        # And the healpix mask
+
+        config.mask_mode = 3
+        mask = get_mask(config)
+        testing.assert_equal(hasattr(mask, 'maskgals'), True)
+        testing.assert_equal(isinstance(mask, Mask), True)
+        testing.assert_equal(isinstance(mask, HPMask), True)
+
+        # When ready, add in test of gen_maskgals()
+
+        # Test the healpix configuration
         testing.assert_equal(mask.nside,2048)
         testing.assert_equal(mask.offset,2100800)
         testing.assert_equal(mask.npix,548292)
 
         # Next test that the fracgood is working properly
         indices = [396440, 445445, 99547, 354028, 516163] #Random indices
-        true_fracgoods = np.array([0,0,0.828125,0.796875,0.828125]) #known fracgoods at indices
-        testing.assert_equal(mask.fracgood_float,1)
-        testing.assert_equal(mask.fracgood.shape[0],548292)
-        testing.assert_equal(mask.fracgood[indices],true_fracgoods)
+        true_fracgoods = np.array([0, 0, 0.828125, 0.796875, 0.828125]) #known fracgoods at indices
+        testing.assert_equal(mask.fracgood_float, 1)
+        testing.assert_equal(mask.fracgood.shape[0], 548292)
+        testing.assert_equal(mask.fracgood[indices], true_fracgoods)
 
         # Next test the compute_radmask() function
         # Note: RA and DECs are in degrees here
-        RAs  = np.array([0.0,293.9,134.9,164.1,281.1,107.5])
-        DECs = np.array([0.0,67.6,159.0,132.4,178.7,35.5]) - 90.0
-        booleans = np.array([False,False,False,False,False,False])#known boolean values for RA/DECs
-        testing.assert_equal(mask.compute_radmask(RAs,DECs),booleans)
 
-        # Next test the set_radmask() function
-        #cluster = make a cluster
-        #mpscale = something
-        #How do I test to see if the function executed at all?
-        #See if the mask.maskgals['MASKED'] attribute exists
-        #See that the maskgals shape has the same shape as the 
-        #cluster RA and DECs.
-        # actually, maskgals size will be 6000 always 
-        #(which is the default config value at least).  
-        #It's independent of the cluster itself -- just a list of random points.
+        RAs = np.array([142.10934, 142.04090, 142.09242, 142.11448, 50.0])
+        Decs = np.array([65.022666, 65.133844, 65.084844, 65.109541, 50.0])
 
+        comp = np.array([True, True, True, True, False])
 
-        # Other masks below... TODO
+        testing.assert_equal(mask.compute_radmask(RAs, Decs), comp)
 
 if __name__=='__main__':
     unittest.main()
