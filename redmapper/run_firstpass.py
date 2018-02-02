a696db6f586b119ac2dfa57853c976774865b6fd
Eli Rykoff <erykoff@slac.stanford.edu>
2018-02-02 10:51:34 -0800

    Still untested, but framework now exists for first 2 stages of redmapper run

---
+++ redmapper/run_firstpass.py
@@ -0,0 +1,177 @@
+from __future__ import print_function
+
+import fitsio
+import numpy as np
+import esutil
+
+from cluster import ClusterCatalog
+from catalog import Catalog
+from background import Background
+from mask import HPMask
+from galaxy import GalaxyCatalog
+from cluster import Cluster
+from cluster import ClusterCatalog
+from depthmap import DepthMap
+from zlambda import Zlambda
+from zlambda import ZlambdaCorrectionPar
+from cluster_runner import ClusterRunner
+
+class RunFirstPass(ClusterRunner):
+    """
+    """
+
+    def _additional_initializations(self, specmode=False):
+        # This is the runmode and where we get the mask/radius config vars from
+        self.runmode = 'firstpass'
+
+        self.read_zreds = True
+        self.zreds_required = False
+        self.specmode = specmode
+        if specmode:
+            self.filetype = 'firstpass_spec'
+        else:
+            self.filetype = 'firstpass'
+
+    def _more_setup(self, *args, **kwargs):
+
+        incat = None
+        try:
+            incat = Catalog.from_fits_file(self.config.seedfile)
+        except:
+            if self.specmode:
+                raise RuntimeError("Must have config.seedfile for run_firstpass in specmode.")
+            elif not self.did_read_zreds:
+                raise RuntimeError("Must have config.seedfile for run_firstpass with no zreds.")
+        if incat is not None:
+            # generate a cluster catalog from incat
+            self.cat = ClusterCatalog.zeros(incat.size,
+                                            zredstr=self.zredstr,
+                                            config=self.config,
+                                            bkg=self.bkg,
+                                            cosmo=self.cosmo)
+
+            self.cat.ra = incat.ra
+            self.cat.dec = incat.dec
+            self.cat.mag = incat.mag
+            self.cat.mag_err = incat.mag_err
+            self.cat.refmag = incat.refmag
+            self.cat.refmag_err = incat.refmag_err
+            self.cat.zred = incat.zred
+            self.cat.zred_e = incat.zred_e
+            self.cat.chisq = incat.chisq
+            self.cat.ebv_mean = incat.ebv
+            self.cat.z_spec_init = incat.zspec
+
+            if self.specmode:
+                self.cat.z_init = incat.zspec
+                self.cat.z = incat.zspec
+            else:
+                self.cat.z_init = incat.zred
+                self.cat.z = incat.zred
+        else:
+            # we must not have a seedfile, and did_read_zreds
+            # so generate a cluster catalog from self.gals
+
+            use,=np.where((self.gals.zred >= self.config.zrange[0]) &
+                          (self.gals.zred <= self.config.zrange[1]))
+
+            if use.size == 0:
+                self.fail = True
+                return
+
+            mstar = self.zredstr.mstar(self.gals.zred[use])
+            mlim = mstars - 2.5 * np.log10(self.config.lval_reference)
+
+            good, = np.where(self.gals.refmag[use] < mlim)
+
+            if good.size == 0:
+                self.fail = True
+                return
+
+            self.cat = ClusterCatalog.zeros(good.size,
+                                            zredstr=self.zredstr,
+                                            config=self.config,
+                                            bkg=self.bkg,
+                                            cosmo=self.cosmo)
+
+            self.cat.ra = self.gals.ra[use[good]]
+            self.cat.dec = self.gals.dec[use[good]]
+            self.cat.mag = self.gals.mag[use[good]]
+            self.cat.mag_err = self.gals.mag_err[use[good]]
+            self.cat.refmag = self.gals.refmag[use[good]]
+            self.cat.refmag_err = self.gals.refmag_err[use[good]]
+            self.cat.zred = self.gals.zred[use[good]]
+            self.cat.zred_e = self.gals.zred_e[use[good]]
+            self.cat.chisq = self.gals.chisq[use[good]]
+
+            self.cat.z_init = self.gals.zred
+            self.cat.z = self.gals.z
+
+            # self.match_cat_to_spectra()
+
+        self.do_lam_plusminus = False
+        self.match_centers_to_galaxies = False
+        self.do_percolation_masking = False
+
+        self.limlum = np.clip(self.config.lval_reference - 0.1, 0.01, None)
+
+        self.maxiter = self.config.firstpass_niter
+        if self.specmode:
+            self.maxiter = 1
+
+        self.min_lambda = self.config.firstpass_minlambda
+
+    def _process_cluster(self, cluster):
+        bad = False
+        iteration = 0
+        done = False
+
+        zuse = cluster.z_init.copy()
+
+        maxmag = cluster.mstar() - 2.5 * np.log10(self.limlum)
+
+        for i in xrange(self.maxiter):
+            if bad:
+                done = True
+                continue
+
+            index, = np.where(cluster.neighbors.refmag < maxmag)
+
+            lam = cluster.calc_richness(self.mask, index=index, calc_err=False)
+
+            if (lam < np.abs(self.config.firstpass_minlambda / cluster.scaleval)):
+                bad = True
+                done = True
+                self._reset_bad_values(cluster)
+                continue
+
+            if i < self.maxiter:
+                # only on first iteration, compute z_lambda
+                # Really, this should be on at most n-1th iteration
+                zlam = Zlambda(cluster)
+                z_lambda, z_lambda_e = zlam.calc_zlambda(cluster.redshift, self.mask,
+                                                         calc_err=True, calcpz=False)
+
+                if z_lambda < self.config.zrange[0] or z_lambda > self.config.zrange[1]:
+                    bad = True
+                    done = True
+                    self._reset_bad_values(cluster)
+                    continue
+
+                #cluster.update_z(z_lambda)
+                cluster.redshift = z_lambda
+
+        cluster.z_lambda = z_lambda
+        cluster.z_lambda_e = z_lambda_e
+        cluster.z_lambda_niter = zlam.niter
+
+        cind = np.argmin(cluster.neighbors.r[index])
+        cluster.chisq = cluster.neighbors.chisq[index[cind]]
+
+        # and record the .z for the next round
+        if (self.specmode):
+            cluster.z = cluster.z_spec_init
+        else:
+            cluster.z = cluster.z_lambda
+
+        # All done
---
+++ redmapper/run_likelihoods.py
@@ -0,0 +1,127 @@
+from __future__ import print_function
+
+import fitsio
+import numpy as np
+import esutil
+
+from cluster import ClusterCatalog
+from catalog import Catalog
+from background import Background
+from mask import HPMask
+from galaxy import GalaxyCatalog
+from cluster import Cluster
+from cluster import ClusterCatalog
+from depthmap import DepthMap
+from zlambda import Zlambda
+from zlambda import ZlambdaCorrectionPar
+from cluster_runner import ClusterRunner
+from utilities import chisq_pdf
+
+class RunLikelihoods(ClusterRunner):
+    """
+    """
+
+    def _additional_initializations(self):
+        self.runmode = 'likelihoods'
+
+        self.read_zreds = False
+        self.zreds_required = False
+        self.filetype = 'like'
+
+    def _more_setup(self, *args, **kwargs):
+
+        self.cat = Catalog.from_fits_file(self.config.catfile)
+
+        try:
+            keepz = kwargs['keepz']
+        except:
+            keepz = False
+
+        if keepz:
+            zrange = self.config.zrange
+            zrange[0] -= self.config.calib_zrange_cushion
+            zrange[0] = zrange[0] if self.zrange[0] > 0.05 else 0.05
+            zrange[1] += self.config.calib_zrange_cushion
+
+        use, = np.where((self.cat.z > zrange[0]) &
+                        (self.cat.z < zrange[1]) &
+                        (self.cat.Lambda > 0.0))
+
+        self.cat = self.cat[use]
+
+        self.do_lam_plusminux = False
+        self.match_centers_to_galaxies = False
+        self.record_members = False
+
+        self.limlum = np.clip(self.config.lval_reference - 0.1, 0.01, None)
+
+    def _reset_bad_values(self, cluster):
+        cluster.lnlamlike = -1e11
+        cluster.lnbcglike = -1e11
+        cluster.lnlike = -1e11
+
+        super(ClusterRunner, self)._reset_bad_values(cluster)
+
+    def _process_cluster(self, cluster):
+
+        maxmag = cluster.mstar() - 2.5*np.log10(self.limlum)
+
+        index, = np.where(cluster.neighbors.refmag < maxmag)
+
+        lam = cluster.calc_richness(self.mask, index=index)
+
+        minrind = np.argmin(cluster.neighbors.r)
+        incut, = np.where((cluster.neighbors.pmem > 0.0) &
+                          (cluster.neighbors.r > cluster.neighbors.r[minrind]))
+
+        if cluster.Lambda < self.config.likelihoods_minlambda or incut.size < 3:
+            # this is a bad cluster
+            self._reset_bad_values(cluster)
+            return
+
+        # compute the cluster member likelihood
+        cluster.lnlamlike = (-cluster.Lambda / cluster.scaleval -
+                              np.sum(np.log(1.0 - cluster.neighbors.pmem[incut])))
+
+        # And the central likelihood
+        if self.config.lnw_cen_sigma <= 0.0:
+            # We do not have a calibration yet
+            cluster.lnbcglike = 0.0
+        else:
+            # First phi_cen
+            mbar = (cluster.mstar() + self.config.wcen_delta0 +
+                    self.config.wcen_delta1 * np.log(cluster.Lambda / self.config.wcen_pivot))
+            phi_cen = ((1. / (np.sqrt(2. * np.pi) * self.config.wcen_sigma_m)) *
+                       np.exp(-0.5 * (cluster.neighbors.refmag[minrind] - mbar)**2. / self.config.wcen_sigma_m**2.))
+
+            # And the zred or chisq filter
+            if self.config.likelihoods_use_zred:
+                if self.zlambda_corr is not None:
+                    zrmod = interpol(self.zlambda_corr.zred_uncorr, self.zlambda_corr.z, cluster.get_z())
+                else:
+                    zrmod = cluster.get_z()
+
+                g = ((1./(np.sqrt(2. * np.pi) * cluster.neighbors.zred_e[minrind])) *
+                     np.exp(-0.5 * (cluster.neighbors.zred[minrind] - zrmod)**2. / cluster.neighbors.zred_e[minrind]**2.))
+            else:
+                # chisq filter
+                g = chisq_pdf(cluster.neighbors.chisq[minrind], self.zredstr.dof)
+
+            # and the w filter
+            lum = 10.**((cluster.mstar() - cluster.neighbors.refmag) / 2.5)
+            u, = np.where((cluster.neighbors.r > 1e-5) & (cluster.neighbors.pmem > 0.0))
+            w = np.alog(np.sum(cluster.neighbors.pmem[u] * lum[u] /
+                               np.sqrt(cluster.neighbors.r[u]**2. + self.config.wcen_rsoft**2.)) / ((1. / cluster.r_lambda) * np.sum(cluster.neighbors.pmem[u] * lum[u])))
+            sig = self.config.lnw_cen_sigma / np.sqrt(((np.clip(cluster.Lambda, None, self.config.wcen_maxlambda)) / cluster.scaleval) / self.config.wcen_pivot)
+            fw = (1. / (np.sqrt(2. * np.pi) * sig)) * np.exp(-0.5 * (np.log(w) - self.config.lnw_cen_mean)**2. / (sig**2.))
+
+            cluster.lnbcglike = np.log(phi_cen * g * fw)
+
+            cluster.lnlike = cluster.lnbcglike + clsuter.lnlamlike
+
+
+    def _postprocess(self):
+        # For this catalog we're cutting on failed likelihood
+
+        use, = np.where(self.cat.lnlamlike > -1e11)
+        self.cat = self.cat[use]
