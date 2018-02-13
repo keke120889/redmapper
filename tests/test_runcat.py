a52b23731bf55c3b01b3d60d55fd54d7c422da99
Eli Rykoff <erykoff@slac.stanford.edu>
2018-02-13 10:16:19 -0800

    Second cluster changed values because the background covers the redshift range now.

--- tests/test_runcat.py
+++ tests/test_runcat.py
@@ -33,16 +33,24 @@ class RuncatTestCase(unittest.TestCase):
         runcat.run(do_percolation_masking=False)
 
         testing.assert_equal(runcat.cat.mem_match_id, [1, 2, 3])
-        testing.assert_almost_equal(runcat.cat.Lambda, [ 23.86299324,  17.39488411, 13.36757088])
-        testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.00936174, 2.4651196])
-        testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506,  0.32121494, 0.22311865])
-        testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.01389629, 0.00969736])
+        testing.assert_almost_equal(runcat.cat.Lambda, [ 23.86299324,  17.3491192, 13.36757088])
+        #testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.00936174, 2.4651196])
+        testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.0184479, 2.4651196])
+        #testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506,  0.32121494, 0.22311865])
+        testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506, 0.3215729 , 0.22311865])
+
+        #testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.01389629, 0.00969736])
+        testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.0139078, 0.00969736])
 
         runcat.run(do_percolation_masking=True)
 
         testing.assert_equal(runcat.cat.mem_match_id, [1, 2, 3])
-        testing.assert_almost_equal(runcat.cat.Lambda, [ 23.86299324,  17.39488411, -1.0])
-        testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.00936174, -1.0])
-        testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506,  0.32121494, -1.0])
-        testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.01389629, -1.0])
+        #testing.assert_almost_equal(runcat.cat.Lambda, [ 23.86299324,  17.39488411, -1.0])
+        testing.assert_almost_equal(runcat.cat.Lambda, [ 23.86299324,  17.3491192, -1.0])
+        #testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.00936174, -1.0])
+        testing.assert_almost_equal(runcat.cat.lambda_e, [ 2.47804546,  2.0184479, -1.0])
+        #testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506,  0.32121494, -1.0])
+        testing.assert_almost_equal(runcat.cat.z_lambda, [ 0.22786506,  0.3215729, -1.0])
+        #testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.01389629, -1.0])
+        testing.assert_almost_equal(runcat.cat.z_lambda_e, [ 0.00629484,  0.0139078, -1.0])
 
