92f7123ced54643b43808d1d95a5228b9d63db29
Eli Rykoff <erykoff@slac.stanford.edu>
2018-02-13 10:13:42 -0800

    Remove old code.  Add in zred background.

--- redmapper/background.py
+++ redmapper/background.py
@@ -3,6 +3,7 @@ import numpy as np
 from catalog import Entry
 import scipy.ndimage as ndi
 # from scipy.interpolate import RegularGridInterpolator
+from utilities import interpol
 
 
 class Background(object):
@@ -20,32 +21,12 @@ class Background(object):
        background filename
     """
 
-    # def _interp(data)
-
-    # def _interp_axis(data, axis, oldx, newx):
-    #     axes = (ax for ax in range(len(data.shape)) if ax != axis) + (axis,)
-    #     data, new_data = np.transpose(data, axes)
-    #     for datum in np.transpose(data, axes).reshape(y*z, len(oldx)):
-    #         new_data[]
-    #     return np.reshape()
-
-    # def __init__(self, filename):
-    #     obkg = Entry.from_fits_file(filename)
-    #     self.zbinsize, self.chisqbinsize, self.refmagbinsize = 0.001, 0.5, 0.01
-    #     self.lnchisqbins = obkg.lnchisqbins
-    #     self.zbins = np.arange(obkg.zrange[0], obkg.zrange[1], self.zbinsize)
-    #     self.chisqbins = np.arange(obkg.chisqrange[0], obkg.chisqrange[1], 
-    #                                                         self.chisqbinsize)
-    #     self.refmagbins = np.arange(obkg.refmagrange[0], obkg.refmagrange[1], 
-    #                                                         self.refmagbinsize)
-    #     self.obkg = obkg
-
     def __init__(self, filename):
         #"""
         #docstring for the constructor
         #"""
         # Get the raw object background from the fits file
-        obkg = Entry.from_fits_file(filename)
+        obkg = Entry.from_fits_file(filename, ext='CHISQBKG')
 
         # Set the bin size in redshift, chisq and refmag spaces
         self.zbinsize = 0.001
@@ -145,13 +126,8 @@ class Background(object):
         refmagindex = np.searchsorted(self.refmagbins, refmag) - 1
         ind = np.clip(np.round((z-zmin)/(self.zbins[1]-zmin)),0, self.zbins.size-1).astype(np.int32)
 
-        #badchisq  = np.where((chisq < self.chisqbins[0])| (chisq > self.chisqbins[-1]))
-        # badchisq, = np.where((chisqindex < 0) | (chisqindex >= self.chisqbins.size))
         badchisq, = np.where((chisq < self.chisqbins[0]) |
                              (chisq > (self.chisqbins[-1] + self.chisqbinsize)))
-        #badrefmag, = np.where((refmag <= self.refmagbins[0])
-        #                     | (refmag >= self.refmagbins[-1]))
-        #badrefmag, = np.where((refmagindex < 0) | (refmagindex >= self.refmagbins.size))
         badrefmag, = np.where((refmag <= self.refmagbins[0]) |
                               (refmag > (self.refmagbins[-1] + self.refmagbinsize)))
 
@@ -167,62 +143,73 @@ class Background(object):
             lookup_vals[np.where((lookup_vals == 0) & (chisq > 5.0))] = np.inf
         return lookup_vals
 
-# # Alternate method of interpolation
-# class AltAltBackground(object):
-#     """Docstring."""
+class ZredBackground(object):
+    """
+    """
 
-#     # @staticmethod
-#     # def _interp(x, y, z, values, xnew=None, ynew=None, znew=None):
-#     #     if xnew is None: xnew = x
-#     #     if ynew is None: ynew = y
-#     #     if znew is None: znew = z
-#     #     xpts, ypts, zpts = np.meshgrid(xnew, ynew, znew)
-#     #     coord = np.vstack((xpts.flatten(), ypts.flatten(), zpts.flatten())).T
-#     #     interp_fn = RegularGridInterpolator((x, y, z), values, 
-#     #                                         bounds_error=False, 
-#     #                                         fill_value=None)
-#     #     result = np.swapaxes(np.reshape(interp_fn(coord), 
-#     #                                 (len(ynew), len(xnew), len(znew))), 0, 1)
-#     #     return np.where(result < 0, 0, result)
+    def __init__(self, filename):
+        obkg = Entry.from_fits_file(filename, ext='ZREDBKG')
 
-#     @staticmethod
-#     def _interp2(x, y, z, values, dx, dy, dz):
-#         scaling, offset = np.array([dx, dy, dz]), np.array([x[0], y[0], z[0]])
-#         coord = np.vstack((arr.flatten() for arr in np.meshgrid(x, y, z)))
-#         idx = (coord.T - offset).T / scaling[(slice(None),) + 
-#                                                 (None,)*(coord.ndim-1)]
-#         new_values = ndi.map_coordinates(values, idx, mode='nearest')
-#         result = np.swapaxes(np.reshape(new_values, 
-#                                     (len(y), len(x), len(z))), 0, 1)
-#         return np.where(result < 0, 0, result)
+        # Will want to make configurable
+        self.refmagbinsize = 0.01
+        self.zredbinsize = 0.001
 
-#     def __init__(self, filename):
-#         obkg = Entry.from_fits_file(filename)
-#         self.zbinsize, self.chisqbinsize, self.refmagbinsize = 0.001, 0.5, 0.01
-        
-#         self.lnchisqbins = obkg.lnchisqbins
-#         self.zbins = np.arange(obkg.zrange[0], obkg.zrange[1], self.zbinsize)
-#         self.chisqbins = np.arange(obkg.chisqrange[0], obkg.chisqrange[1], 
-#                                                             self.chisqbinsize)
-#         self.refmagbins = np.arange(obkg.refmagrange[0], obkg.refmagrange[1], 
-#                                                             self.refmagbinsize)
-#         self.obkg = obkg
-        
-#         # self.sigma_g = AltBackground._interp(obkg.refmagbins, obkg.chisqbins, 
-#         #                                   obkg.zbins, obkg.sigma_g, 
-#         #                                   xnew=self.refmagbins, 
-#         #                                   ynew=self.chisqbins, 
-#         #                                   znew=self.zbins)
-#         # self.sigma_lng = AltBackground._interp(obkg.refmagbins, obkg.lnchisqbins, 
-#         #                                     obkg.zbins, obkg.sigma_lng, 
-#         #                                     xnew=self.refmagbins, 
-#         #                                     znew=self.zbins)
+        # Create the refmag bins
+        refmagbins = np.arange(obkg.refmagrange[0], obkg.refmagrange[1], self.refmagbinsize)
+        nrefmagbins = refmagbins.size
 
-#         self.sigma_g = AltBackground._interp2(self.refmagbins, self.chisqbins,
-#                                 self.zbins, obkg.sigma_g, obkg.refmagbinsize,
-#                                 obkg.chisqbinsize, obkg.zbinsize)
-#         self.sigma_lng = AltBackground._interp2(self.refmagbins, self.lnchisqbins,
-#                                 self.zbins, obkg.sigma_lng, obkg.refmagbinsize,
-#                                 obkg.lnchisqbinsize, obkg.zbinsize)
-#         self.n = np.sum(self.sigma_g, axis=1) * self.chisqbinsize
+        # Leave the zred bins the same
+        nzredbins = obkg.zredbins.size
 
+        # Set up arrays to populate
+        # sigma_g_new = np.zeros((nzredbins, nrefmagbins))
+        sigma_g_new = np.zeros((nrefmagbins, nzredbins))
+
+        floor = np.min(obkg.sigma_g)
+
+        for i in xrange(nzredbins):
+            #sigma_g_new[i, :] = np.clip(interpol(obkg.sigma_g[i, :], obkg.refmagbins, refmagbins), floor, None)
+            sigma_g_new[:, i] = np.clip(interpol(obkg.sigma_g[:, i], obkg.refmagbins, refmagbins), floor, None)
+
+        sigma_g = sigma_g_new.copy()
+
+        # And update zred
+        zredbins = np.arange(obkg.zredrange[0], obkg.zredrange[1], self.zredbinsize)
+        nzredbins = zredbins.size
+
+        #sigma_g_new = np.zeros((nzredbins, nrefmagbins))
+        sigma_g_new = np.zeros((nrefmagbins, nzredbins))
+
+        for i in xrange(nrefmagbins):
+            #sigma_g_new[:, i] = np.clip(interpol(sigma_g[:, i], obkg.zredbins, zredbins), floor, None)
+            sigma_g_new[i, :] = np.clip(interpol(sigma_g[i, :], obkg.zredbins, zredbins), floor, None)
+
+        self.zredbins = zredbins
+        self.zredrange = obkg.zredrange
+        self.zred_index = 0
+        self.refmag_index = 1
+        self.refmagbins = refmagbins
+        self.refmagrange = obkg.refmagrange
+        self.sigma_g = sigma_g_new
+
+    def sigma_g_lookup(self, zred, refmag):
+        """
+        """
+
+        zredindex = np.searchsorted(self.zredbins, zred) - 1
+        refmagindex = np.searchsorted(self.refmagbins, refmag) - 1
+
+        badzred, = np.where((zredindex < 0) |
+                            (zredindex >= self.zredbins.size))
+        zredindex[badzred] = 0
+        badrefmag, = np.where((refmagindex < 0) |
+                              (refmagindex >= self.refmagbins.size))
+        refmagindex[badrefmag] = 0
+
+        #lookup_vals = self.sigma_g[zredindex, refmagindex]
+        lookup_vals = self.sigma_g[refmagindex, zredindex]
+
+        lookup_vals[badzred] = np.inf
+        lookup_vals[badrefmag] = np.inf
+
+        return lookup_vals
