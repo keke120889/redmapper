9a00b43687ec0ff5a00946c90375615c0b864d98
Eli Rykoff <erykoff@stanford.edu>
2018-05-13 10:23:29 -0700

    Testing for redsequencecal.  Incomplete.

---
+++ tests/test_redsequencecal.py
@@ -0,0 +1,64 @@
+from __future__ import division, absolute_import, print_function
+from past.builtins import xrange
+
+import matplotlib
+matplotlib.use('Agg')
+
+import unittest
+import os
+import shutil
+import numpy.testing as testing
+import numpy as np
+import fitsio
+import tempfile
+from numpy import random
+
+from redmapper.configuration import Configuration
+from redmapper.calibration import RedSequenceCalibrator
+
+class SelectRedSequenceCalTestCase(unittest.TestCase):
+    def test_selectredsequencecal(self):
+        file_path = 'data_for_tests'
+        conf_filename = 'testconfig.yaml'
+        config = Configuration(os.path.join(file_path, conf_filename))
+        config.calib_use_pcol = True
+
+        config.zrange = [0.1, 0.2]
+
+        test_dir = tempfile.mkdtemp(dir='./', prefix='TestRedmapper-')
+        config.outpath = test_dir
+
+        config.parfile = os.path.join(config.outpath, '%s_testpars.fit' % (config.outbase))
+
+        gal_filename = 'test_dr8_trainredseq_gals.fit'
+        galfile = os.path.join(file_path, gal_filename)
+
+        redsequencecal = RedSequenceCalibrator(config, galfile)
+        redsequencecal.run(doRaise=False)
+
+        # make sure that the pars and the plots were made
+        self.assertTrue(os.path.isfile(config.parfile))
+        for i in xrange(config.nmag - 1):
+            plotfile = os.path.join(config.outpath, config.plotpath, '%s_%s-%s.png' % (config.outbase, config.bands[i], config.bands[i + 1]))
+            self.assertTrue(plotfile)
+        plotfile = os.path.join(config.outpath, config.plotpath, '%s_zred_plots.png' % (config.outbase))
+        self.assertTrue(plotfile)
+        plotfile = os.path.join(config.outpath, config.plotpath, '%s_zred2_plots.png' % (config.outbase))
+        self.assertTrue(plotfile)
+        plotfile = os.path.join(config.outpath, config.plotpath, '%s_offdiags.png' % (config.outbase))
+        self.assertTrue(plotfile)
+
+        # Read in the pars and check numbers
+        pars = fitsio.read(config.parfile, ext=1)
+        testing.assert_almost_equal(pars[0]['pivotmag'], np.array([17.11333275, 18.5870018]), 5)
+        testing.assert_almost_equal(pars[0]['medcol'], np.array([[1.89056611, 0.92165649, 0.40067941, 0.33493668],
+                                                                 [2.00396729, 1.23734939, 0.47769779, 0.32707551]]), 5)
+        testing.assert_almost_equal(pars[0]['c01'], np.array([0.92390829, 1.07899368, 1.23799872]), 5)
+        testing.assert_almost_equal(pars[0]['slope01'], np.array([-0.00473213, -0.02403165]), 5)
+        testing.assert_almost_equal(pars[0]['covmat_amp'][1:3, 1:3, :], np.array([[[0.0006989, 0.0017012], [-0.00118965, -0.000662]], [[-0.00118965, -0.000662 ], [0.0025, 0.0025]]]), 5)
+
+        if os.path.exists(test_dir):
+            shutil.rmtree(test_dir, True)
+
+if __name__=='__main__':
+    unittest.main()
