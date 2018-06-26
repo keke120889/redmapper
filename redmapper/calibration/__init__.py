e812b2bb6629500ec4c82104dc92f95558a63b3b
Eli Rykoff <erykoff@stanford.edu>
2018-06-26 09:48:07 -0700

    Add WcenCalibrator

--- redmapper/calibration/__init__.py
+++ redmapper/calibration/__init__.py
@@ -2,3 +2,5 @@ from __future__ import division, absolute_import, print_function
 
 from .selectspecred import SelectSpecRedGalaxies
 from .redsequencecal import RedSequenceCalibrator
+from .centeringcal import WcenCalibrator
+
