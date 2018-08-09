d9f9771d3a6518c5615b95ce16bcdd7166f96247
Eli Rykoff <erykoff@stanford.edu>
2018-08-09 07:09:21 -0700

    Add maskgals config parameters

--- redmapper/configuration.py
+++ redmapper/configuration.py
@@ -158,6 +158,11 @@ class Configuration(object):
     mask_mode = ConfigField(default=0, required=True)
     max_maskfrac = ConfigField(default=0.2, required=True)
 
+    maskgal_ngals = ConfigField(default=6000, required=True)
+    maskgal_rad_stepsize = ConfigField(default=0.1, required=True)
+    maskgal_dmag_extra = ConfigField(default=0.3, required=True)
+    maskgal_zred_err = ConfigField(default=0.02, required=True)
+
     dldr_gamma = ConfigField(default=0.6, required=True)
     rsig = ConfigField(default=0.05, required=True)
     chisq_max = ConfigField(default=20.0, required=True)
