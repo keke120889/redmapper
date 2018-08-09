d0b638983b7b21d8902f7eb0b416debb4afedb49
Eli Rykoff <erykoff@stanford.edu>
2018-08-09 07:09:41 -0700

    Add generation of maskgals

--- redmapper/mask.py
+++ redmapper/mask.py
@@ -5,10 +5,13 @@ import esutil
 import fitsio
 import healpy as hp
 import numpy as np
+import os
 from scipy.special import erf
+import scipy.integrate
 
 from .catalog import Catalog,Entry
 from .utilities import TOTAL_SQDEG, SEC_PER_DEG, astro_to_sphere, calc_theta_i, apply_errormodels
+from .utilities import make_lockfile, sample_from_pdf, chisq_pdf, schechter_pdf, nfw_pdf
 
 class Mask(object):
     """
@@ -27,6 +30,7 @@ class Mask(object):
     #   the correct type.  How is this typically done?
 
     def __init__(self, config):
+        self.config = config
         try:
             self.read_maskgals(config.maskgalfile)
         except:
@@ -35,11 +39,146 @@ class Mask(object):
 
     def calc_radmask(self, *args, **kwargs): pass
     def read_maskgals(self, maskgalfile):
+
+        make_maskgals = False
+        lockfile = maskgalfile + '.lock'
+        locktest = make_lockfile(lockfile, block=True, maxtry=300, waittime=2)
+        if not locktest:
+            raise RuntimeError("Could not get a lock to read/write maskgals!")
+
+        if not os.path.isfile(maskgalfile):
+            # We don't have the maskgals...generate them
+            self._gen_maskgals(maskgalfile)
+
+        # Read the maskgals
         self.maskgals = Catalog.from_fits_file(maskgalfile)
-    def gen_maskgals(self):
-        # this needs to be written to generate maskgals if not from file
-        # Tom-where would we generate them from?
-        pass
+
+        # Clear lockfile
+        os.remove(lockfile)
+
+    def _gen_maskgals(self, maskgalfile):
+        """
+        """
+
+        minrad = np.clip(np.floor(10.*self.config.percolation_r0 * (3./100.)**self.config.percolation_beta) / 10., None, 0.5)
+        maxrad = np.ceil(10.*self.config.percolation_r0 * (300./100.)**self.config.percolation_beta) / 10.
+
+        nradbins = np.ceil((maxrad - minrad) / self.config.maskgal_rad_stepsize).astype(np.int32) + 1
+        radbins = np.arange(nradbins, dtype=np.float32) * self.config.maskgal_rad_stepsize + minrad
+
+        nmag = self.config.nmag
+        ncol = nmag - 1
+
+        ngals = self.config.maskgal_ngals  # * nsamples
+
+        maskgals = Catalog.zeros(ngals, dtype=[('r', 'f4'),
+                                               ('phi', 'f4'),
+                                               ('x', 'f4'),
+                                               ('y', 'f4'),
+                                               ('m', 'f4'),
+                                               ('refmag', 'f4'),
+                                               ('refmag_obs', 'f4'),
+                                               ('refmag_obs_err', 'f4'),
+                                               ('chisq', 'f4'),
+                                               ('cwt', 'f4'),
+                                               ('nfw', 'f4'),
+                                               ('dzred', 'f4'),
+                                               ('zwt', 'f4'),
+                                               ('lumwt', 'f4'),
+                                               ('limmag', 'f4'),
+                                               ('limmag_dered', 'f4'),
+                                               ('exptime', 'f4'),
+                                               ('m50', 'f4'),
+                                               ('eff', 'f4'),
+                                               ('w', 'f4'),
+                                               ('theta_r', 'f4', nradbins),
+                                               ('mark', np.bool),
+                                               ('radbins', 'f4', nradbins),
+                                               ('nin', 'f4', nradbins),
+                                               ('nin_orig', 'f4', nradbins),
+                                               ('zp', 'f4'),
+                                               ('ebv', 'f4'),
+                                               ('extinction', 'f4'),
+                                               ('nsig', 'f4')])
+
+        maskgals['radbins'] = np.tile(radbins, maskgals.size).reshape(maskgals.size, nradbins)
+
+        # Generate chisq
+        maskgals.chisq = sample_from_pdf(chisq_pdf, [0.0, self.config.chisq_max],
+                                         self.config.chisq_max / 10000.,
+                                         maskgals.size, k=ncol)
+        # Generate mstar
+        maskgals.m = sample_from_pdf(schechter_pdf,
+                                     [-2.5*np.log10(10.0),
+                                       -2.5*np.log10(self.config.lval_reference) + self.config.maskgal_dmag_extra],
+                                     0.002, maskgals.size,
+                                     alpha=self.config.calib_lumfunc_alpha, mstar=0.0)
+        # Generate nfw(r)
+        maskgals.r = sample_from_pdf(nfw_pdf,
+                                     [0.001, maxrad],
+                                     0.001, maskgals.size, radfactor=True)
+
+        # Generate phi
+        maskgals.phi = 2. * np.pi * np.random.random(size=maskgals.size)
+
+        # Precompute x/y
+        maskgals.x = maskgals.r * np.cos(maskgals.phi)
+        maskgals.y = maskgals.r * np.sin(maskgals.phi)
+
+        # Compute weights to go with these values
+
+        # Chisq weight
+        maskgals.cwt = chisq_pdf(maskgals.chisq, ncol)
+
+        # Nfw weight
+        maskgals.nfw = nfw_pdf(maskgals.r, radfactor=True)
+
+        # luminosity weight
+
+        # We just choose a reference mstar for the normalization code
+        mstar = 19.0
+        normmag = mstar - 2.5 * np.log10(self.config.lval_reference)
+        steps = np.arange(10.0, normmag, 0.01)
+        f = schechter_pdf(steps, alpha=self.config.calib_lumfunc_alpha, mstar=mstar)
+        n = scipy.integrate.simps(f, steps)
+        maskgals.lumwt = schechter_pdf(maskgals.m + mstar, mstar=mstar, alpha=self.config.calib_lumfunc_alpha) / n
+
+        # zred weight
+        maskgals.dzred = np.random.normal(loc=0.0, scale=self.config.maskgal_zred_err, size=maskgals.size)
+        maskgals.zwt = (1. / (np.sqrt(2.*np.pi) * self.config.maskgal_zred_err)) * np.exp(-(maskgals.dzred**2.) / (2.*self.config.maskgal_zred_err**2.))
+
+        # Radial function
+        for i, rad in enumerate(radbins):
+            inside, = np.where((maskgals.r <= rad) & (maskgals.m < -2.5*np.log10(self.config.lval_reference)))
+            maskgals.nin_orig[:, i] = inside.size
+
+            if self.config.rsig <= 0.0:
+                theta_r = np.ones(maskgals.size)
+            else:
+                theta_r = 0.5 + 0.5*erf((rad - maskgals.r) / (np.sqrt(2.)*self.config.rsig))
+            maskgals.theta_r[:, i] = theta_r
+
+            inside2, = np.where(maskgals.m < -2.5*np.log10(self.config.lval_reference))
+            maskgals.nin[:, i] = np.sum(theta_r[inside2])
+
+        # And save it
+
+        hdr = fitsio.FITSHDR()
+        hdr['version'] = 5
+        hdr['r0'] = self.config.percolation_r0
+        hdr['beta'] = self.config.percolation_beta
+        hdr['stepsize'] = self.config.maskgal_rad_stepsize
+        hdr['nmag'] = self.config.nmag
+        hdr['ngals'] = self.config.maskgal_ngals
+        hdr['chisqmax'] = self.config.chisq_max
+        hdr['lvalref'] = self.config.lval_reference
+        hdr['extra'] = self.config.maskgal_dmag_extra
+        hdr['alpha'] = self.config.calib_lumfunc_alpha
+        hdr['rsig'] = self.config.rsig
+        hdr['zrederr'] = self.config.maskgal_zred_err
+
+        maskgals.to_fits_file(maskgalfile, clobber=True, header=hdr)
+
 
     def set_radmask(self, cluster):
         """
