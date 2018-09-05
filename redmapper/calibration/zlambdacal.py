6409c74d0281546249a8d9554a8c396f31ee206e
Eli Rykoff <erykoff@stanford.edu>
2018-09-05 16:11:54 -0700

    Calibrate zlambda corrections

---
+++ redmapper/calibration/zlambdacal.py
@@ -0,0 +1,268 @@
+from __future__ import division, absolute_import, print_function
+
+import os
+import numpy as np
+import fitsio
+import time
+import scipy.optimize
+import copy
+
+from ..configuration import Configuration
+from ..redsequence import RedSequenceColorPar
+from ..galaxy import GalaxyCatalog
+from ..cluster import ClusterCatalog
+from ..zlambda import ZlambdaCorrectionPar
+from ..utilities import make_nodes, CubicSpline, interpol
+from ..fitters import MedZFitter
+from ..catalog import Entry
+
+class ZLambdaFitter(object):
+    def __init__(self, nodes, slope_nodes, redshifts, dzs, redshift_errs, loglambdas):
+        self._nodes = np.atleast_1d(nodes)
+        self._slope_nodes = np.atleast_1d(slope_nodes)
+        self._redshifts = np.atleast_1d(redshifts)
+        self._dzs = np.atleast_1d(dzs)
+        self._redshift_err2s = np.atleast_1d(redshift_errs)**2.
+        self._loglambdas = np.atleast_1d(loglambdas)
+
+        self._n_nodes = self._nodes.size
+        self._n_slope_nodes = self._slope_nodes.size
+
+        if self._redshifts.size != self._dzs.size:
+            raise ValueError("Number of redshifts must be equal to dzs")
+        if self._redshifts.size != self._redshift_err2s.size:
+            raise ValueError("Number of redshifts must be equal to redshift_errs")
+        if self._redshifts.size != self._loglambdas.size:
+            raise ValueError("Number of redshifts must be equal to loglambdas")
+
+    def fit(self, p0_delta, p0_slope, p0_scatter,
+            fit_delta=False, fit_slope=False, fit_scatter=False,
+            min_scatter=0.0):
+        """
+        """
+        self._fit_delta = fit_delta
+        self._fit_slope = fit_slope
+        self._fit_scatter = fit_scatter
+        self._min_scatter = min_scatter
+
+        ctr = 0
+        p0 = np.array([])
+        if self._fit_delta:
+            self._delta_index = 0
+            ctr += self._n_nodes
+            p0 = np.append(p0, p0_delta)
+        if self._fit_slope:
+            self._slope_index = ctr
+            ctr += self._n_slope_nodes
+            p0 = np.append(p0, p0_slope)
+        if self._fit_scatter:
+            self._scatter_index = ctr
+            ctr += self._n_slope_nodes
+            p0 = np.append(p0, p0_scatter)
+
+        if ctr == 0:
+            raise ValueError("Must select at least one of fit_delta, fit_slope, fit_scatter")
+
+        # Precompute
+        if not self._fit_delta:
+            spl = CubicSpline(self._nodes, p0_delta)
+            self._gdelta = spl(self._redshifts)
+        if not self._fit_slope:
+            spl = CubicSpline(self._slope_nodes, p0_slope)
+            self._gslope = spl(self._redshifts)
+        if not self._fit_scatter:
+            spl = CubicSpline(self._slope_nodes, p0_scatter)
+            self._gscatter = np.clip(spl(self._redshifts), self._min_scatter, None)
+
+        pars = scipy.optimize.fmin(self, p0, disp=False)
+
+        retval = []
+        if self._fit_delta:
+            retval.append(pars[self._delta_index: self._delta_index + self._n_nodes])
+        if self._fit_slope:
+            retval.append(pars[self._slope_index: self._slope_index + self._n_slope_nodes])
+        if self._fit_scatter:
+            retval.append(pars[self._scatter_index: self._scatter_index + self._n_slope_nodes])
+
+        return retval
+
+    def __call__(self, pars):
+        """
+        """
+        if self._fit_delta:
+            spl = CubicSpline(self._nodes, pars[self._delta_index: self._delta_index + self._n_nodes])
+            gdelta = spl(self._redshifts)
+        else:
+            gdelta = self._gdelta
+
+        if self._fit_slope:
+            spl = CubicSpline(self._slope_nodes, pars[self._slope_index: self._slope_index + self._n_slope_nodes])
+            gslope = spl(self._redshifts)
+        else:
+            gslope = self._gslope
+
+        if self._fit_scatter:
+            spl = CubicSpline(self._slope_nodes, pars[self._scatter_index: self._scatter_index + self._n_slope_nodes])
+            gscatter = np.clip(spl(self._redshifts), self._min_scatter, None)
+        else:
+            gscatter = self._gscatter
+
+        vartot = gscatter**2. + self._redshift_err2s
+        gdi = (1. / np.sqrt(2.*np.pi*vartot)) * np.exp(-(self._dzs -
+                                                         (gdelta + gslope*self._loglambdas))**2. / (2.*vartot))
+
+        vals = np.log(gdi)
+        bad, = np.where(~np.isfinite(vals))
+        vals[bad] = -100.0
+
+        t = -np.sum(vals)
+
+        if self._fit_scatter:
+            if pars[self._scatter_index: self._scatter_index + self._n_slope_nodes].min() < self._min_scatter:
+                t += 10000
+
+        return t
+
+class ZLambdaCalibrator(object):
+    """
+    """
+
+    def __init__(self, config, corrslope=False):
+        self.config = config
+        self.corrslope = corrslope
+
+    def run(self):
+        """
+        """
+
+        cat = ClusterCatalog.from_catfile(self.config.catfile, cosmo=self.config.cosmo)
+
+        # We set the redshift according to the initial spec redshift for training
+        cat.z = cat.z_spec_init
+
+        use, = np.where((cat.Lambda/cat.scaleval > self.config.calib_zlambda_minlambda) &
+                        (cat.scaleval > 0.0) &
+                        (cat.maskfrac < self.config.max_maskfrac))
+        cat = cat[use]
+
+        nodes = make_nodes(self.config.zrange, self.config.calib_zlambda_nodesize)
+        slope_nodes = make_nodes(self.config.zrange, self.config.calib_zlambda_slope_nodesize)
+
+        # we have two runs, first "<zlambda|ztrue>" the second "<ztrue|zlambda>".
+
+        out_struct = Entry(np.zeros(1, dtype=[('niter_true', 'i4'),
+                                              ('offset_z', 'f4', nodes.size),
+                                              ('offset', 'f4', nodes.size),
+                                              ('offset_true', 'f4', (nodes.size, self.config.calib_zlambda_correct_niter)),
+                                              ('slope_z', 'f4', slope_nodes.size),
+                                              ('slope', 'f4', slope_nodes.size),
+                                              ('slope_true', 'f4', (slope_nodes.size, self.config.calib_zlambda_correct_niter)),
+                                              ('scatter', 'f4', slope_nodes.size),
+                                              ('scatter_true', 'f4', (slope_nodes.size, self.config.calib_zlambda_correct_niter)),
+                                              ('zred_uncorr', 'f4', nodes.size)]))
+
+        out_struct.niter_true = self.config.calib_zlambda_correct_niter
+        out_struct.offset_z = nodes
+        out_struct.slope_z = slope_nodes
+
+        for fitType in range(2):
+            if fitType == 0:
+                print("Fitting zlambda corrections...")
+                nziter = 1
+            else:
+                print("Fitting ztrue corrections...")
+                nziter = self.config.calib_zlambda_correct_niter
+
+            # Make a backup copy of the catalog
+            cat_orig = copy.deepcopy(cat)
+
+            # ziter is the iterations stored for the correction
+            # (these are needed for the ztrue corrections because
+            #  we don't know ztrue at first, we need to zero-in on it)
+            for ziter in range(nziter):
+                # get the starting points
+
+                delta_vals = np.zeros(nodes.size)
+                slope_vals = np.zeros(slope_nodes.size)
+                scatter_vals = np.zeros(slope_nodes.size) + 0.001
+
+                # We have another iteration to remove outliers
+                for outlier_iter in range(2):
+                    if outlier_iter == 0:
+                        # Straightforward outlier removal
+                        use, = np.where(np.abs(cat.z - cat.z_lambda) < 3.0*cat.z_lambda_e)
+                    else:
+                        # Add on the estimate of the scatter
+                        spl = CubicSpline(slope_nodes, scatter_vals)
+                        scatter = spl(cat.z)
+                        use, = np.where(np.abs(cat.z - cat.z_lambda) < 3.0*np.sqrt(cat.z_lambda_e**2. + scatter**2.))
+
+                    if fitType == 0:
+                        z_fit = cat.z[use]
+                    else:
+                        z_fit = cat.z_lambda[use]
+
+                    dzs = cat.z[use] - cat.z_lambda[use]
+                    zerrs = cat.z_lambda_e[use]
+                    llam = np.log((cat.Lambda[use] / cat.scaleval[use]) / self.config.zlambda_pivot)
+
+                    fitter = ZLambdaFitter(nodes, slope_nodes, z_fit, dzs, zerrs, llam)
+                    delta_vals, = fitter.fit(delta_vals, slope_vals, scatter_vals, fit_delta=True)
+                    if self.corrslope:
+                        slope_vals, = fitter.fit(delta_vals, slope_vals, scatter_vals, fit_slope=True)
+
+                    scatter_vals, = fitter.fit(delta_vals, slope_vals, scatter_vals, fit_scatter=True)
+
+                    if self.corrslope:
+                        delta_vals, slope_vals, scatter_vals = fitter.fit(delta_vals, slope_vals, scatter_vals, fit_delta=True, fit_slope=True, fit_scatter=True)
+                    else:
+                        delta_vals, scatter_vals = fitter.fit(delta_vals, slope_vals, scatter_vals, fit_delta=True, fit_slope=False, fit_scatter=True)
+
+                    # Record the fit values in the output structure
+
+                    if (fitType == 0):
+                        # Record offset, slope, scatter
+                        out_struct.offset = delta_vals
+                        out_struct.slope = slope_vals
+                        out_struct.scatter = scatter_vals
+                    else:
+                        # Record offset_true, slope_true, scatter_true
+                        out_struct.offset_true[:, ziter] = delta_vals
+                        out_struct.slope_true[:, ziter] = slope_vals
+                        out_struct.scatter_true[:, ziter] = scatter_vals
+
+                # Run the corrections if we're doing ztrue fitType == 1
+
+                if fitType == 1:
+                    zlambda_corr = ZlambdaCorrectionPar(pars=out_struct,
+                                                        zrange=np.array([self.config.zrange[0] - 0.02,
+                                                                         self.config.zrange[1] + 0.07]),
+                                                        zbinsize=self.config.zlambda_binsize,
+                                                        zlambda_pivot=self.config.zlambda_pivot)
+
+                    # reset the catalog before applying correction
+                    cat = copy.deepcopy(cat_orig)
+
+                    for cluster in cat:
+                        # Need to apply correction here
+                        zlam, zlam_e = zlambda_corr.apply_correction(cluster.Lambda, cluster.z_lambda, cluster.z_lambda_e)
+                        cluster.z_lambda = zlam
+                        cluster.z_lambda_e = zlam_e
+
+
+        # Need to do the zred uncorr calibration, blah.
+        medfitter = MedZFitter(nodes, cat_orig.z_lambda, cat_orig.zred)
+        p0 = nodes
+        zred_uncorr = medfitter.fit(p0)
+
+        out_struct.zred_uncorr = zred_uncorr
+
+        # And now save the file
+
+        hdr = fitsio.FITSHDR()
+        hdr['ZRANGE0'] = self.config.zrange[0] - 0.02
+        hdr['ZRANGE1'] = self.config.zrange[1] + 0.07
+        hdr['ZBINSIZE'] = self.config.zlambda_binsize
+        hdr['ZLAMPIV'] = self.config.zlambda_pivot
+
+        out_struct.to_fits_file(self.config.zlambdafile, header=hdr)
---
+++ tests/test_zlambdacal.py
@@ -0,0 +1,77 @@
+from __future__ import division, absolute_import, print_function
+from past.builtins import xrange
+
+#import matplotlib
+#matplotlib.use('Agg')
+
+import unittest
+import os
+import shutil
+import numpy.testing as testing
+import numpy as np
+import fitsio
+import tempfile
+from numpy import random
+
+from redmapper.configuration import Configuration
+from redmapper.calibration import ZLambdaCalibrator
+
+class ZLambdaCalTestCase(unittest.TestCase):
+    def test_zlambdacal(self):
+        file_path = 'data_for_tests'
+        conf_filename = 'testconfig.yaml'
+        config = Configuration(os.path.join(file_path, conf_filename))
+
+        config.zrange = [0.1, 0.2]
+
+        test_dir = tempfile.mkdtemp(dir='./', prefix='TestRedmapper-')
+        config.outpath = test_dir
+
+        config.catfile = os.path.join(file_path, 'test_zlamcal_cat.fit')
+        config.zlambdafile = os.path.join(test_dir, 'test_zlambdafile.fits')
+
+        zlambdacal = ZLambdaCalibrator(config, corrslope=False)
+        zlambdacal.run()
+
+        # Make sure the file is there
+        self.assertTrue(os.path.isfile(config.zlambdafile))
+
+        # Read in the pars and check numbers
+
+        pars = fitsio.read(config.zlambdafile, ext=1)
+        self.assertEqual(pars['niter_true'], 3)
+        testing.assert_almost_equal(pars[0]['offset_z'], np.array([0.1, 0.14, 0.20]))
+        testing.assert_almost_equal(pars[0]['offset'], np.array([0.0012012032,
+                                                                 -6.4453372e-05,
+                                                                 -0.0023969179]))
+        testing.assert_almost_equal(pars[0]['offset_true'][:, 0], np.array([0.00184188,
+                                                                           0.00080734,
+                                                                           -0.00375052]))
+        testing.assert_almost_equal(pars[0]['offset_true'][:, 1], np.array([5.90356824e-04,
+                                                                            6.35077959e-05,
+                                                                            -8.10466707e-04]))
+        testing.assert_almost_equal(pars[0]['offset_true'][:, 2], np.array([-1.05633946e-04,
+                                                                             6.05580608e-05,
+                                                                             -2.35550746e-04]))
+        testing.assert_almost_equal(pars[0]['slope_z'], np.array([0.1, 0.2]))
+        testing.assert_almost_equal(pars[0]['slope'], np.array([0.0, 0.0]))
+        testing.assert_almost_equal(pars[0]['slope_true'][:, 0], np.array([0.0, 0.0]))
+        testing.assert_almost_equal(pars[0]['scatter'], np.array([0.00274064, 0.00333985]))
+        testing.assert_almost_equal(pars[0]['scatter_true'][:, 0], np.array([0.00257572,
+                                                                             0.00269449]))
+        testing.assert_almost_equal(pars[0]['scatter_true'][:, 1], np.array([2.79080211e-07,
+                                                                             1.77075050e-03]))
+        testing.assert_almost_equal(pars[0]['scatter_true'][:, 2], np.array([7.67152073e-07,
+                                                                             1.73576328e-03]))
+        testing.assert_almost_equal(pars[0]['zred_uncorr'], np.array([0.09942016,
+                                                                      0.14075805,
+                                                                      0.2043011]))
+
+        # Clean up
+        if os.path.exists(test_dir):
+            shutil.rmtree(test_dir, True)
+
+if __name__=='__main__':
+    unittest.main()
+
+
