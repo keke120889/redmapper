578b931ef7ed5ebfafeaeea2d96334c5513b743b
Eli Rykoff <erykoff@stanford.edu>
2018-09-05 16:12:17 -0700

    Calibrate zlambda corrections

--- redmapper/calibration/__init__.py
+++ redmapper/calibration/__init__.py
@@ -3,4 +3,4 @@ from __future__ import division, absolute_import, print_function
 from .selectspecred import SelectSpecRedGalaxies
 from .redsequencecal import RedSequenceCalibrator
 from .centeringcal import WcenCalibrator
-
+from .zlambdacal import ZLambdaCalibrator
