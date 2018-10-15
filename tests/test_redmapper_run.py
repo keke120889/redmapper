bc65f1097de79824e9688de74fbc50c0907527d1
Eli Rykoff <erykoff@stanford.edu>
2018-10-15 14:55:40 -0700

    Update for values with better mask

--- tests/test_redmapper_run.py
+++ tests/test_redmapper_run.py
@@ -65,9 +65,9 @@ class RedmapperRunTestCase(unittest.TestCase):
         cat = Catalog.from_fits_file(os.path.join(config.outpath, '%s_final.fit' % (config.d.outbase)))
 
         # Spot checks to look for regressions
-        testing.assert_equal(cat.size, 27)
+        testing.assert_equal(cat.size, 24)
         self.assertGreater(cat.Lambda.min(), 3.0)
-        testing.assert_array_almost_equal(cat.Lambda[0: 3], np.array([24.061308, 17.94406319, 7.73848534]))
+        testing.assert_array_almost_equal(cat.Lambda[0: 3], np.array([24.412172, 17.944063, 7.738485]))
 
         # And check that the members are all accounted for...
         mem = Catalog.from_fits_file(os.path.join(config.outpath, '%s_final_members.fit' % (config.d.outbase)))
