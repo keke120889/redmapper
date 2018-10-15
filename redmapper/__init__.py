908a48d05c6b25b87f387c7a26eaa810f5d54df3
Eli Rykoff <erykoff@stanford.edu>
2018-10-15 14:32:47 -0700

    Add calibration, catalog maker, and limit threads in computations

--- redmapper/__init__.py
+++ redmapper/__init__.py
@@ -1,5 +1,11 @@
 from __future__ import division, absolute_import, print_function
 
+import os
+
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+
 from ._version import __version__, __version_info__
 
 version = __version__
@@ -12,7 +18,7 @@ from .redsequence import RedSequenceColorPar
 from .chisq_dist import compute_chisq
 from .background import Background, ZredBackground, BackgroundGenerator, ZredBackgroundGenerator
 from .cluster import Cluster, ClusterCatalog
-from .galaxy import Galaxy, GalaxyCatalog
+from .galaxy import Galaxy, GalaxyCatalog, GalaxyCatalogMaker
 from .mask import Mask, HPMask, get_mask
 from .zlambda import Zlambda, ZlambdaCorrectionPar
 from .cluster_runner import ClusterRunner
@@ -30,3 +36,4 @@ from .redmapper_run import RedmapperRun
 from .depth_fitting import DepthLim
 from .plotting import SpecPlot
 from .utilities import read_members
+from . import calibration
