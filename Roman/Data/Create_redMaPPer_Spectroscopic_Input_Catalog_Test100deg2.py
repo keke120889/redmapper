9780bef47f20be6af2f2cb478a3115d0ea79139e
I-Hsuan Li <ihsuan@midway3-login4.rcc.local>
2025-02-12 13:19:33 -0600

    New spectroscopic galaxies

--- Roman/Data/Create_redMaPPer_Spectroscopic_Input_Catalog_Test100deg2.py
+++ Roman/Data/Create_redMaPPer_Spectroscopic_Input_Catalog_Test100deg2.py
@@ -1,7 +1,7 @@
 #
 # This is for making the redMaPPer spectroscopic input galaxy catalog (2000 galaxies in the 100 deg^2 region).
 # 20250127 IHsuan starts
-# 20250129        finishes
+# 20250212        finishes
 #
 ########################################
 import h5py
@@ -14,20 +14,25 @@ cat       = h5py.File('/project/chihway/chto/Roman/combine_v2.h5', 'r')
 catselect = np.load('/project/chihway/chto/Roman/batchrun/Rd_select.npy', mmap_mode='r')
 
 # choose the 100 deg^2 region
-ra        = np.array(cat['catalog/gold/ra'][:][catselect],  dtype='f8')
-dec       = np.array(cat['catalog/gold/dec'][:][catselect], dtype='f8')
+ra           = np.array(cat['catalog/gold/ra'][:][catselect],  dtype='f8')
+dec          = np.array(cat['catalog/gold/dec'][:][catselect], dtype='f8')
 FilterRA     = np.logical_and(130.0 <= ra, ra <= 140.0)
 FilterDEC    = np.logical_and(20.0 <= dec, dec <= 30.0)
 FilterRegion = np.logical_and(FilterRA, FilterDEC)
 
+z            = np.array(cat['catalog/gold/z'][:][catselect],  dtype='f8')
+FilterZ      = np.logical_and(0.2 <= z, z <= 1.8)
+
+FilterAll    = np.logical_and(FilterRegion, FilterZ)
+
 # Randomly select 2000 galaxies
-RandomSample = np.sort(random.sample(range(0, np.sum(FilterRegion)), 2000))
+RandomSample = np.sort(random.sample(range(0, np.sum(FilterAll)), 2000))
 
 np.save('/project/chihway/ihsuan/Roman/Data/Create_redMaPPer_Spectroscopic_Input_Catalog_Test100deg2.npy', RandomSample)
 
-ztrue     = np.array(cat['catalog/gold/z'][:][catselect][FilterRegion][RandomSample],   dtype='f4')
-ra        = np.array(cat['catalog/gold/ra'][:][catselect][FilterRegion][RandomSample],  dtype='f8')
-dec       = np.array(cat['catalog/gold/dec'][:][catselect][FilterRegion][RandomSample], dtype='f8')
+ztrue     = np.array(cat['catalog/gold/z'][:][catselect][FilterAll][RandomSample],   dtype='f4')
+ra        = np.array(cat['catalog/gold/ra'][:][catselect][FilterAll][RandomSample],  dtype='f8')
+dec       = np.array(cat['catalog/gold/dec'][:][catselect][FilterAll][RandomSample], dtype='f8')
 z_err     = np.ones(np.shape(dec)) * 0.00001
 
 """
--- redmapper/_version.py
+++ redmapper/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1200+g0804fd4'
-__version_tuple__ = version_tuple = (0, 1, 'dev1200', 'g0804fd4')
+__version__ = version = '0.1.dev1201+g0f4ce78.d20250212'
+__version_tuple__ = version_tuple = (0, 1, 'dev1201', 'g0f4ce78.d20250212')
